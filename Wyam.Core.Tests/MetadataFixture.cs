9a3c8807565638a2a3d6e01876276f150f737cfe
Dave Glick <dave@somedave.com>
2015-02-17 14:41:25 -0500

    Added a bunch of additional metadata tests

--- Wyam.Core.Tests/MetadataFixture.cs
+++ Wyam.Core.Tests/MetadataFixture.cs
@@ -12,11 +12,34 @@ namespace Wyam.Core.Tests
     [TestFixture]
     public class MetadataFixture
     {
-        // TODO: Test for setting value for static metadata
-        // TODO: Test for setting value for dynamic metadata
-        // TODO: Test for reading value for static metadata
-        // TODO: Test for reading value for dynamic metadata
-        // TODO: Tests for various static metadata methods (Get(), TryGet(), etc.)
+        [Test]
+        public void CanSetStaticMetadata()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+
+            // When
+            metadata.Set("A", "a");
+
+            // Then
+            Assert.AreEqual("a", metadata["A"]);
+        }
+        
+        [Test]
+        public void CanSetDynamicMetadata()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            dynamic dyn = metadata;
+
+            // When
+            dyn.A = "a";
+
+            // Then
+            Assert.AreEqual("a", metadata["A"]);
+        }
 
         [Test]
         public void IsReadOnlyDoesNotAllowEditsForStaticMetadata()
@@ -90,6 +113,170 @@ namespace Wyam.Core.Tests
 
             // Then
             Assert.IsNull(value);
+        }
+
+        [Test]
+        public void ContainsReturnsTrueForValidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+
+            // When
+            bool contains = metadata.Contains("A");
+
+            // Then
+            Assert.IsTrue(contains);
+        }
+
+        [Test]
+        public void ContainsReturnsFalseForInvalidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+
+            // When
+            bool contains = metadata.Contains("B");
+
+            // Then
+            Assert.IsFalse(contains);
+        }
+
+        [Test]
+        public void TryGetReturnsTrueForValidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+
+            // When
+            object value;
+            bool contains = metadata.TryGet("A", out value);
+
+            // Then
+            Assert.IsTrue(contains);
+            Assert.AreEqual("a", value);
+        }        
+
+        [Test]
+        public void TryGetReturnsFalseForInvalidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+
+            // When
+            object value;
+            bool contains = metadata.TryGet("B", out value);
+
+            // Then
+            Assert.IsFalse(contains);
+            Assert.AreEqual(null, value);
+        }
+
+        [Test]
+        public void IndexerReturnsValidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata["A"] = "a";
+
+            // When
+            object value = metadata["A"];
+
+            // Then
+            Assert.AreEqual("a", value);
+        } 
+
+        [Test]
+        public void IndexerReturnsNullForInvalidValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata["A"] = "a";
+
+            // When
+            object value = metadata["B"];
+
+            // Then
+            Assert.AreEqual(null, value);
+        } 
+
+        [Test]
+        public void CloneContainsPreviousValues()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+            Metadata clone = metadata.Clone();
+            clone.Set("B", "b");
+
+            // When
+            object value = clone.Get("A");
+
+            // Then
+            Assert.AreEqual("a", value);
+        }
+
+        [Test]
+        public void ClonedDoesNotContainNewValues()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+            Metadata clone = metadata.Clone();
+            clone.Set("B", "b");      
+
+            // When
+            object value = metadata.Get("B");
+
+            // Then
+            Assert.AreEqual(null, value);
+        }
+
+        [Test]
+        public void CloneContainsNewValues()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+            Metadata clone = metadata.Clone();
+            clone.Set("B", "b");      
+
+            // When
+            object value = clone.Get("B");
+
+            // Then
+            Assert.AreEqual("b", value);
+        }
+
+        [Test]
+        public void CloneReplacesValue()
+        {
+            // Given
+            Engine engine = new Engine();
+            Metadata metadata = new Metadata(engine);
+            metadata.Set("A", "a");
+            Metadata clone = metadata.Clone();
+            clone.Set("A", "b");      
+
+            // When
+            object value = metadata.Get("A");
+            object cloneValue = clone.Get("A");
+
+            // Then
+            Assert.AreEqual("a", value);
+            Assert.AreEqual("b", cloneValue);
         }
     }
 }
