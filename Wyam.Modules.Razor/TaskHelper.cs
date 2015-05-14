53d423895bf8aaaf7bd4683a82742155268e1201
Dave Glick <dave@daveaglick.com>
2015-05-14 17:56:55 -0400

    Removing all the Razor vNext bits before starting over (reverted from commit 8d1bf65937ec15dca7a51965830f0b8c3ccf8714)

---
+++ Wyam.Modules.Razor/TaskHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyam.Modules.Razor
+{
+    // From https://github.com/aspnet/Mvc/blob/dev/src/Microsoft.AspNet.Mvc.Common/TaskHelper.cs
+    internal static class TaskHelper
+    {
+        /// <summary>
+        /// Waits for the task to complete and throws the first faulting exception if the task is faulted.
+        /// It preserves the original stack trace when throwing the exception.
+        /// </summary>
+        /// <remarks>
+        /// Invoking this method is equivalent to calling Wait() on the <paramref name="task" /> if it is not completed.
+        /// </remarks>
+        public static void WaitAndThrowIfFaulted(Task task)
+        {
+            task.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Waits for the task to complete and throws the first faulting exception if the task is faulted.
+        /// It preserves the original stack trace when throwing the exception.
+        /// </summary>
+        /// <remarks>
+        /// Invoking this method is equivalent to calling <see cref="Task{TResult}.Result"/> on the
+        /// <paramref name="task"/> if it is not completed.
+        /// </remarks>
+        public static TVal WaitAndThrowIfFaulted<TVal>(Task<TVal> task)
+        {
+            return task.GetAwaiter().GetResult();
+        }
+    }
+}
