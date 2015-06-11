ef38e56a69e11aebfad0bb0de6e6a33d50187d73
Dave Glick <dave@daveaglick.com>
2015-06-11 17:48:02 -0400

    Changing NuGet versions to prerelease

--- Wyam.Core/Properties/AssemblyInfo.cs
+++ Wyam.Core/Properties/AssemblyInfo.cs
@@ -37,3 +37,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
