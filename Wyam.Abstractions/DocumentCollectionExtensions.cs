23dd8f3cb306a4a3da81b2a4f86bb9a14979c753
Dave Glick <dave@daveaglick.com>
2015-06-26 18:18:49 -0400

    Created a ToLookup<T> method that makes working with groups of documents that share a metadata value much easier (for example, all the blog posts with a given tag)

--- Wyam.Abstractions/DocumentCollectionExtensions.cs
+++ Wyam.Abstractions/DocumentCollectionExtensions.cs
@@ -22,5 +22,26 @@ namespace Wyam.Abstractions
         {
             return documents.Where(x => keys.Any(x.ContainsKey));
         }
+
+        public static ILookup<T, IDocument> ToLookup<T>(this IEnumerable<IDocument> documents, string key)
+        {
+            // Get a mapping of all documents with the key to their values
+            Dictionary<IDocument, IEnumerable<T>> documentValues = documents
+                .Distinct()
+                .Select(x =>
+                {
+                    IEnumerable<T> values;
+                    return new KeyValuePair<IDocument, IEnumerable<T>>(x, x.MetadataAs<IEnumerable<T>>().TryGetValue(key, out values) ? values : null);
+                })
+                .Where(x => x.Value != null)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            // Now invert the dictionary to a lookup of values to documents
+            return documentValues
+                .SelectMany(x => x.Value)
+                .Distinct()
+                .SelectMany(x => documentValues.Where(y => y.Value.Contains(x)).Select(y => new KeyValuePair<T, IDocument>(x, y.Key)))
+                .ToLookup(x => x.Key, x => x.Value);
+        }
     }
 }
---
+++ Wyam.Core.Tests/DocumentCollectionExtensionsFixture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Wyam.Abstractions;
+
+namespace Wyam.Core.Tests
+{
+    [TestFixture]
+    public class DocumentCollectionExtensionsFixture
+    {
+        [Test]
+        public void ToLookupOfIntReturnsCorrectLookup()
+        {
+            // Given
+            Engine engine = new Engine();
+            IDocument a = new Document(new Metadata(engine))
+                .Clone("a", new[] { new KeyValuePair<string, object>("Numbers", new [] { 1, 2, 3 }) });
+            IDocument b = new Document(new Metadata(engine))
+                .Clone("b", new[] { new KeyValuePair<string, object>("Numbers", new [] { 2, 3, 4 }) });
+            IDocument c = new Document(new Metadata(engine))
+                .Clone("c", new[] { new KeyValuePair<string, object>("Numbers", 3) });
+            IDocument d = new Document(new Metadata(engine))
+                .Clone("d", new[] { new KeyValuePair<string, object>("Numbers", "4") });
+            List<IDocument> documents = new List<IDocument>() { a, b, c, d };
+
+            // When
+            ILookup<int, IDocument> lookup = documents.ToLookup<int>("Numbers");
+
+            // Then
+            Assert.AreEqual(4, lookup.Count);
+            CollectionAssert.AreEqual(new[] { a }, lookup[1]);
+            CollectionAssert.AreEqual(new[] { a, b }, lookup[2]);
+            CollectionAssert.AreEqual(new[] { a, b, c }, lookup[3]);
+            CollectionAssert.AreEqual(new[] { b, d }, lookup[4]);
+        }
+
+        [Test]
+        public void ToLookupOfStringReturnsCorrectLookup()
+        {
+            // Given
+            Engine engine = new Engine();
+            IDocument a = new Document(new Metadata(engine))
+                .Clone("a", new[] { new KeyValuePair<string, object>("Numbers", new[] { 1, 2, 3 }) });
+            IDocument b = new Document(new Metadata(engine))
+                .Clone("b", new[] { new KeyValuePair<string, object>("Numbers", new[] { 2, 3, 4 }) });
+            IDocument c = new Document(new Metadata(engine))
+                .Clone("c", new[] { new KeyValuePair<string, object>("Numbers", 3) });
+            IDocument d = new Document(new Metadata(engine))
+                .Clone("d", new[] { new KeyValuePair<string, object>("Numbers", "4") });
+            List<IDocument> documents = new List<IDocument>() { a, b, c, d };
+
+            // When
+            ILookup<string, IDocument> lookup = documents.ToLookup<string>("Numbers");
+
+            // Then
+            Assert.AreEqual(4, lookup.Count);
+            CollectionAssert.AreEqual(new[] { a }, lookup["1"]);
+            CollectionAssert.AreEqual(new[] { a, b }, lookup["2"]);
+            CollectionAssert.AreEqual(new[] { a, b, c }, lookup["3"]);
+            CollectionAssert.AreEqual(new[] { b, d }, lookup["4"]);
+        }
+    }
+}
