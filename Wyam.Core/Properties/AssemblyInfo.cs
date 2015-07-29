23b51edb260c17c98841ee37d7c29e1f04676f4b
Dave Glick <dave@daveaglick.com>
2015-07-29 15:44:05 -0400

    Changing back to prerelease due to NuGet.Packaging dependency

--- Wyam.Core/Properties/AssemblyInfo.cs
+++ Wyam.Core/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.7.0.0")]
 [assembly: AssemblyFileVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7.0")]  // Also update RELEASE file when changing version
+[assembly: AssemblyInformationalVersion("0.7.0-beta")]  // Also update RELEASE file when changing version
