baed80da1fd7a08f7f5fe0afab17a69de83c6a23
Dave Glick <dave@daveaglick.com>
2015-08-11 13:08:09 -0400

    Adds thread safety to the cache objects per #50

--- Wyam.Modules.Html.Tests/AutoLinkFixture.cs
+++ Wyam.Modules.Html.Tests/AutoLinkFixture.cs
@@ -90,6 +90,50 @@ namespace Wyam.Modules.Html.Tests
         }
 
         [Test]
+        public void AddsLinkWithAlternateQuerySelector()
+        {
+            // Given
+            string input = @"<html>
+                    <head>
+                        <title>Foobar</title>
+                    </head>
+                    <body>
+                        <h1>Title</h1>
+                        <baz>This is some Foobar text</baz>
+                        <p>This is some Foobar text</p>
+                    </body>
+                </html>";
+            string output = @"<html><head>
+                        <title>Foobar</title>
+                    </head>
+                    <body>
+                        <h1>Title</h1>
+                        <baz>This is some <a href=""http://www.google.com"">Foobar</a> text</baz>
+                        <p>This is some Foobar text</p>
+                    
+                </body></html>".Replace("\r\n", "\n");
+            IDocument document = Substitute.For<IDocument>();
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+            document.Stream.Returns(stream);
+            string content = null;
+            document
+                .When(x => x.Clone(Arg.Any<string>()))
+                .Do(x => content = x.Arg<string>()); ;
+            AutoLink autoLink = new AutoLink(new Dictionary<string, string>()
+            {
+                { "Foobar", "http://www.google.com" }
+            }).SetQuerySelector("baz");
+
+            // When
+            autoLink.Execute(new[] { document }, null).ToList();  // Make sure to materialize the result list
+
+            // Then
+            document.Received().Clone(Arg.Any<string>());
+            Assert.AreEqual(output, content);
+            stream.Dispose();
+        }
+
+        [Test]
         public void AddsLinkWhenContainerHasChildElements()
         {
             // Given
