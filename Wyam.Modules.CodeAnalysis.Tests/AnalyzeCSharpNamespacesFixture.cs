766264cada5eb4abb65ae087ef8eca0c9a7ca9f2
Dave Glick <dave@daveaglick.com>
2015-09-23 20:52:09 -0400

    More work on namespace tests for #70

--- Wyam.Modules.CodeAnalysis.Tests/AnalyzeCSharpNamespacesFixture.cs
+++ Wyam.Modules.CodeAnalysis.Tests/AnalyzeCSharpNamespacesFixture.cs
@@ -62,7 +62,7 @@ namespace Wyam.Modules.CodeAnalysis.Tests
             // Then
             CollectionAssert.AreEquivalent(new[] { string.Empty, "Foo", "Baz", "Bar" }, test.Metadata.Select(x => x["Name"]));
             CollectionAssert.AreEquivalent(new [] { "Foo", "Bar" },
-                GetChildMetadata<string>(test.Metadata, x => (string)x["Name"] == string.Empty, "NestedNamespaces", "Name"));
+                test.GetChildMetadata<string>(x => (string)x["Name"] == string.Empty, "NestedNamespaces", "Name"));
             test.Stream.Dispose();
         }
 
@@ -91,7 +91,7 @@ namespace Wyam.Modules.CodeAnalysis.Tests
             // Then
             CollectionAssert.AreEquivalent(new[] { string.Empty, "Foo", "Baz", "Bar" }, test.Metadata.Select(x => x["Name"]));
             CollectionAssert.AreEquivalent(new[] { "Baz", "Bar" },
-                GetChildMetadata<string>(test.Metadata, x => (string)x["Name"] == "Foo", "NestedNamespaces", "Name"));
+                test.GetChildMetadata<string>(x => (string)x["Name"] == "Foo", "NestedNamespaces", "Name"));
             test.Stream.Dispose();
         }
 
@@ -141,6 +141,92 @@ namespace Wyam.Modules.CodeAnalysis.Tests
             test.Stream.Dispose();
         }
 
+        [Test]
+        public void NestedNamespacesReferenceParents()
+        {
+            // Given
+            string code = @"
+                namespace Foo
+                {
+                }
+
+                namespace Foo.Bar
+                {
+                }
+            ";
+            TestObjects test = new TestObjects(code);
+
+            // When
+            test.Module.Execute(new[] { test.Document }, test.Context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            Assert.AreEqual("Foo", test.GetMetadata<IDocument>(x => (string)x["Name"] == "Bar", "ParentNamespace").Get("Name"));
+            test.Stream.Dispose();
+        }
+
+        [Test]
+        public void NamespacesContainTypes()
+        {
+            // Given
+            string code = @"
+                namespace Foo
+                {
+                    class Red
+                    {
+                    }
+                }
+
+                namespace Foo.Bar
+                {
+                    class Blue
+                    {
+                    }
+
+                    class Green
+                    {
+                    }
+                }
+            ";
+            TestObjects test = new TestObjects(code);
+
+            // When
+            test.Module.Execute(new[] { test.Document }, test.Context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            CollectionAssert.AreEquivalent(new [] { "Red" }, 
+                test.GetMetadata<IEnumerable<IDocument>>(x => (string)x["Name"] == "Foo", "Types").Select(x => x.Get("Name")));
+            CollectionAssert.AreEquivalent(new[] { "Blue", "Green" },
+                test.GetMetadata<IEnumerable<IDocument>>(x => (string)x["Name"] == "Bar", "Types").Select(x => x.Get("Name")));
+            test.Stream.Dispose();
+        }
+
+        [Test]
+        public void NamespacesContainNestedTypes()
+        {
+            // Given
+            string code = @"
+                namespace Foo
+                {
+                    class Blue
+                    {
+                        class Green
+                        {
+                        }
+                    }
+                }
+            ";
+            TestObjects test = new TestObjects(code);
+
+            // When
+            test.Module.Execute(new[] { test.Document }, test.Context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            CollectionAssert.AreEquivalent(new[] { "Blue", "Green" },
+                test.GetMetadata<IEnumerable<IDocument>>(x => (string)x["Name"] == "Foo", "Types").Select(x => x.Get("Name")));
+            test.Stream.Dispose();
+        }
+
+        // Document property only implements GetEnumerator() and Get(string, object)
         private class TestObjects
         {
             public IDocument Document { get; } = Substitute.For<IDocument>();
@@ -158,20 +244,30 @@ namespace Wyam.Modules.CodeAnalysis.Tests
                     {
                         IDocument newDocument = Substitute.For<IDocument>();
                         newDocument.GetEnumerator().Returns(((IEnumerable<KeyValuePair<string, object>>)x[0]).GetEnumerator());
+                        newDocument.Get(Arg.Any<string>(), Arg.Any<object>()).Returns(y =>
+                        {
+                            KeyValuePair<string, object> kvp = ((IEnumerable<KeyValuePair<string, object>>)x[0]).FirstOrDefault(z => z.Key == (string)y[0]);
+                            return kvp.Equals(default(KeyValuePair<string, object>)) ? y[1] : kvp.Value;
+                        });
                         Metadata.Add(((IEnumerable<KeyValuePair<string, object>>)x[0]).ToDictionary(y => y.Key, y => y.Value));
                         return newDocument;
                     });
             }
-        }
 
-        // Gets metadata Name values for children given a parent IDocument and a metadata key containing an IEnumerable<IDocument>
-        private IEnumerable<T> GetChildMetadata<T>(List<Dictionary<string, object>> metadata,
-            Func<Dictionary<string, object>, bool> parentSelector, string documentsKey, string childKey)
-        {
-            return ((IEnumerable<IDocument>)(metadata.Single(parentSelector)[documentsKey]))
-                .Select(x => x.First(y => y.Key == childKey).Value)
-                .Cast<T>();
-        } 
+            // Gets a specific metadata value from the metadata collection
+            public T GetMetadata<T>(Func<Dictionary<string, object>, bool> metadataSelector, string metadataKey)
+            {
+                return (T)Metadata.Single(metadataSelector)[metadataKey];
+            }
+
+            // Gets metadata Name values for children given a parent IDocument and a metadata key containing an IEnumerable<IDocument>
+            public IEnumerable<T> GetChildMetadata<T>(Func<Dictionary<string, object>, bool> parentSelector, string documentsKey, string childKey)
+            {
+                return GetMetadata<IEnumerable<IDocument>>(parentSelector, documentsKey)
+                    .Select(x => x.Get(childKey))
+                    .Cast<T>();
+            }
+        }
     }
 
 }
