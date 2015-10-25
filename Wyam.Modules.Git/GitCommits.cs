990536a5ab3ae0ac071084fa882cf67350a55687
Patrick Kranz <patrick-kranz@live.de>
2015-10-25 18:01:08 +0100

    Added GitCommits Module

---
+++ Wyam.Modules.Git/GitCommits.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+using LibGit2Sharp;
+using System.IO;
+using Wyam.Common.Configuration;
+
+namespace Wyam.Modules.Git
+{
+    /// <summary>
+    /// This Module adds an Array with all the CommitInformations that are related with this file to the Metadata.
+    /// </summary>
+    public class GitCommits : GitBase
+    {
+        private readonly string _metadataName;
+
+        public GitCommits(string metadataName)
+        {
+            _metadataName = metadataName;
+        }
+
+        public GitCommits() : this("Commits")
+        {
+
+        }
+
+        public override IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            var repositoryLocation = Repository.Discover(context.InputFolder);
+            if (repositoryLocation == null)
+                throw new ArgumentException("No git repository found");
+
+            using (Repository repository = new Repository(repositoryLocation))
+            {
+
+                var data = GetCommitInformation(repository);
+                var lookup = data.ToLookup(x => x.Path.ToLower());
+                return inputs.Select(x =>
+                {
+                    string relativePath = GetRelativePath(Path.GetDirectoryName(Path.GetDirectoryName(repositoryLocation.ToLower())), x.Source.ToLower()); // yes we need to do it twice
+                    if (!lookup.Contains(relativePath))
+                        return x;
+
+                    var commitsOfFile = lookup[relativePath].ToArray();
+     
+                    return x.Clone(new[]
+                    {
+                        new KeyValuePair<string, object>(_metadataName, commitsOfFile)
+                    });
+                }).ToArray(); // Don't do it lazy or Commit is disposed.
+            }
+        }
+
+        private string GetRelativePath(string reposotoryLocation, string source)
+        {
+            FilePath repositoryLocationPath = new FilePath(reposotoryLocation);
+            FilePath sourcePath = new FilePath(source);
+            FilePath relativePath = sourcePath.RelativeTo(repositoryLocationPath);
+            return relativePath.ToWindowsPath();
+        }
+
+
+        private class SingelUserDistinction : IEqualityComparer<Author>
+        {
+            public bool Equals(Author x, Author y)
+            {
+                return x.Email == y.Email;
+            }
+
+            public int GetHashCode(Author obj)
+            {
+                return obj.Email.GetHashCode();
+            }
+        }
+
+    }
+}
