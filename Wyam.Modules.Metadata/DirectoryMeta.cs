f4a4acf81dc38b7bf97a29c9d574571340be48ca
Patrick Kranz <patrick-kranz@live.de>
2015-10-25 07:44:52 +0100

    Running spell checker

--- Wyam.Modules.Metadata/DirectoryMeta.cs
+++ Wyam.Modules.Metadata/DirectoryMeta.cs
@@ -12,43 +12,43 @@ using Wyam.Common.Pipelines;
 namespace Wyam.Modules.Metadata
 {
     /// <summary>
-    /// This Module allows to specifiy metadata for Multiple documents in one file.
+    /// This Module allows to specify metadata for Multiple documents in one file.
     /// </summary>
     /// <remarks>
-    /// This Module uses the folder structor of the Source property in the documents to
+    /// This Module uses the folder structure of the Source property in the documents to
     /// determinate which files share there Metadata.
     /// 
-    /// This can lead to unexpected behavior if Source is not a path (filesystem).
+    /// This can lead to unexpected behavior if Source is not a path (file system).
     /// 
-    /// You can define multiple filenames that will be reagareded as MetadataFiles.
+    /// You can define multiple filenames that will be regarded as MetadataFiles.
     /// Use <see cref="WithMetadataFile(string, bool, bool)"/> to define some.
     /// 
-    /// This Module ueses whatever Metadata is present in the documents that
-    /// are regareded as MetadataFiles. You can use e.g. Yaml to generate the Metadata.
+    /// This Module uses whatever Metadata is present in the documents that
+    /// are regarded as MetadataFiles. You can use e.g. Yaml to generate the Metadata.
     /// 
-    /// Only Documents in the pipeline will be searchd for MetadataFiles. So both the
+    /// Only Documents in the pipeline will be searched for MetadataFiles. So both the
     /// MetadataFiles and the documents that should get the Metadata should be processed by this Module.
     /// 
-    /// The order you register new MetadataFilenames determns the priority which value
+    /// The order you register new MetadataFilenames determents the priority which value
     /// will be used if there is a conflict. Use this list to find out which value will be assigned:
     /// 
     /// * Local value
-    /// * MetadataFiles in the same locataion.
+    /// * MetadataFiles in the same location.
     ///   - The MetadataFiel that was registered first has the highest priority
-    /// * Metadatafiles in parent firectorys (only if those have the inherited switch set)
-    ///   - The MetadataFile that is less levels abouve has the highest priorety
+    /// * Metadatafiles in parent directory (only if those have the inherited switch set)
+    ///   - The MetadataFile that is less levels above has the highest priority
     /// 
     /// For every MetadataFile you define you can pass multiple Options.
     ///  * **inherited** (default = <c>false</c>)
     ///                   If this is true metadata will be set on Documents in sub folders.
     /// 
     ///  * **override**  (default = <c>false</c>)
-    ///                  Normaly a local value has the highest priorety. With this option set to
+    ///                  Normally a local value has the highest priority. With this option set to
     ///                  true, Metadata in this MetadataFile can override local values in a document.
     ///                  But it will not override values from other MetadataFiles that have 
-    ///                  a higher priorety
+    ///                  a higher priority
     /// 
-    /// By default files that define metadata will be filterd by this Module
+    /// By default files that define metadata will be filtered by this Module
     /// and does not exist in the output. To change this use PreserveMetadataFiles.
     /// </remarks>
     public class DirectoryMeta : IModule
@@ -113,7 +113,7 @@ namespace Wyam.Modules.Metadata
                 .Where(x => _preserveMetadataFiles || !(_metadataFile.Any(isMetadata => isMetadata.MetadataFileName.Invoke<bool>(x, context)))) // ignore files that define Metadata if not preserved
                 .Select(x =>
                 {
-                    // First add the inhired Metadata to temp dictenary.
+                    // First add the inhered Metadata to temp dictionary.
                     string dir = Path.GetDirectoryName(x.Source);
                     List<string> sourcePathes = new List<string>();
                     while (dir.StartsWith(context.InputFolder))
@@ -122,7 +122,7 @@ namespace Wyam.Modules.Metadata
                         dir = Path.GetDirectoryName(dir);
                     }
 
-                    HashSet<string> overriddenKeys = new HashSet<string>(); // we need to know which keys we may override if they are overriden.
+                    HashSet<string> overriddenKeys = new HashSet<string>(); // we need to know which keys we may override if they are overridden.
                     List<KeyValuePair<string, object>> newMetadataKey = new List<KeyValuePair<string, object>>();
 
                     bool firstLevel = true;
@@ -134,7 +134,7 @@ namespace Wyam.Modules.Metadata
                             foreach (var metadataEntry in metadataDictinary[path])
                             {
                                 if (!firstLevel && !metadataEntry.MetadataFileEntry.Inherited)
-                                    continue; // If we are not in the same directory and inherited isne't activeated 
+                                    continue; // If we are not in the same directory and inherited isn't activated 
 
                                 foreach (var keyValuePair in metadataEntry.Metadata)
                                 {
@@ -143,10 +143,10 @@ namespace Wyam.Modules.Metadata
 
                                     if (x.Metadata.ContainsKey(keyValuePair.Key)
                                         && !metadataEntry.MetadataFileEntry.Override)
-                                        continue; // The value alredy exists and this metadatafile has no override
+                                        continue; // The value already exists and this MetadataFile has no override
 
                                     // We can add the value.
-                                    overriddenKeys.Add(keyValuePair.Key); // no other MetadataFile may overide it.
+                                    overriddenKeys.Add(keyValuePair.Key); // no other MetadataFile may override it.
 
                                     newMetadataKey.Add(keyValuePair);
                                 }
