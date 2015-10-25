234e1af8e5ec037afac5d8a17b76728e29c07ea7
Dave Glick <dave@daveaglick.com>
2015-10-25 11:34:42 -0400

    Renamed Fork to BranchConcat and added a Where predicate re #87

---
+++ Wyam.Core.Tests/Modules/BranchConcatFixture.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Wyam.Core.Modules;
+
+namespace Wyam.Core.Tests.Modules
+{
+    [TestFixture]
+    public class BranchConcatFixture
+    {
+        [Test]
+        public void BranchConcatResultsInCorrectCounts()
+        {
+            // Given
+            Engine engine = new Engine();
+            engine.Trace.AddListener(new TestTraceListener());
+            CountModule a = new CountModule("A")
+            {
+                AdditionalOutputs = 1
+            };
+            CountModule b = new CountModule("B")
+            {
+                AdditionalOutputs = 2
+            };
+            CountModule c = new CountModule("C")
+            {
+                AdditionalOutputs = 3
+            };
+            engine.Pipelines.Add(a, new BranchConcat(b), c);
+
+            // When
+            engine.Execute();
+
+            // Then
+            Assert.AreEqual(1, a.ExecuteCount);
+            Assert.AreEqual(1, b.ExecuteCount);
+            Assert.AreEqual(1, c.ExecuteCount);
+            Assert.AreEqual(1, a.InputCount);
+            Assert.AreEqual(2, b.InputCount);
+            Assert.AreEqual(8, c.InputCount);
+            Assert.AreEqual(2, a.OutputCount);
+            Assert.AreEqual(6, b.OutputCount);
+            Assert.AreEqual(32, c.OutputCount);
+        }
+
+        [Test]
+        public void BranchConcatWithPredicateResultsInCorrectCounts()
+        {
+            // Given
+            Engine engine = new Engine();
+            engine.Trace.AddListener(new TestTraceListener());
+            CountModule a = new CountModule("A")
+            {
+                AdditionalOutputs = 1
+            };
+            CountModule b = new CountModule("B")
+            {
+                AdditionalOutputs = 2
+            };
+            CountModule c = new CountModule("C")
+            {
+                AdditionalOutputs = 3
+            };
+            engine.Pipelines.Add(a, new BranchConcat(b).Where((x, y) => x.Content == "1"), c);
+
+            // When
+            engine.Execute();
+
+            // Then
+            Assert.AreEqual(1, a.ExecuteCount);
+            Assert.AreEqual(1, b.ExecuteCount);
+            Assert.AreEqual(1, c.ExecuteCount);
+            Assert.AreEqual(1, a.InputCount);
+            Assert.AreEqual(1, b.InputCount);
+            Assert.AreEqual(5, c.InputCount);
+            Assert.AreEqual(2, a.OutputCount);
+            Assert.AreEqual(3, b.OutputCount);
+            Assert.AreEqual(20, c.OutputCount);
+        }
+    }
+}
---
+++ Wyam.Core/Modules/BranchConcat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common;
+using Wyam.Common.Configuration;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+
+namespace Wyam.Core.Modules
+{
+    // Executes a sequence of modules against the input and concatenates their results and the original input
+    public class BranchConcat : IModule
+    {
+        private readonly IModule[] _modules;
+        private Func<IDocument, IExecutionContext, bool> _predicate;
+
+        public BranchConcat(params IModule[] modules)
+        {
+            _modules = modules;
+        }
+
+        // The delegate should return a bool
+        public BranchConcat Where(DocumentConfig predicate)
+        {
+            Func<IDocument, IExecutionContext, bool> currentPredicate = _predicate;
+            _predicate = currentPredicate == null
+                ? (Func<IDocument, IExecutionContext, bool>)(predicate.Invoke<bool>)
+                : ((x, c) => currentPredicate(x, c) && predicate.Invoke<bool>(x, c));
+            return this;
+        }
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            IEnumerable<IDocument> documents = _predicate == null ? inputs : inputs.Where(x => _predicate(x, context));
+            return inputs.Concat(context.Execute(_modules, documents));
+        }
+    }
+}
