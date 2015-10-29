cfd40280b2a571d19fcc0c896fb16c0d72d33922
Patrick Kranz <patrick-kranz@live.de>
2015-10-29 12:19:06 +0100

    Added Module Xmp to Images

---
+++ Wyam.Modules.Images/Xmp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+
+namespace Wyam.Modules.Images
+{
+    public class Xmp : IModule
+    {
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            inputs.Select(x =>
+            {
+
+                var extractedMetadata = MetadataExtractor.ImageMetadataReader.ReadMetadata(x.GetStream())
+                    .OfType<MetadataExtractor.Formats.Xmp.XmpDirectory>()
+                    .FirstOrDefault();
+                if (extractedMetadata.HasError)
+                {
+                    using (context.Trace.WithIndent().Warning($"Errors in {x.Source}"))
+                    {
+                        foreach (var error in extractedMetadata.Errors)
+                        {
+                            context.Trace.Warning(error);
+                        }
+                    }
+                }
+
+            });
+        }
+    }
+}
