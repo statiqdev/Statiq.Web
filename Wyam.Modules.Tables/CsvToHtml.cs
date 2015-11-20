d3bc3f65e91f70e42b0d78cfdf2aad3bca110c18
Patrick Kranz <patrick-kranz@live.de>
2015-11-20 12:17:27 +0100

    Added TablesProject

---
+++ Wyam.Modules.Tables/CsvToHtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+
+namespace Wyam.Modules.Tables
+{
+    public class CsvToHtml : IModule
+    {
+        private bool _firstLineHeader = false;
+
+        public CsvToHtml WithHeader()
+        {
+            _firstLineHeader = true;
+            return this;
+        }
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            return inputs.AsParallel().Select(x =>
+            {
+                try
+                {
+                    Tabular.Csv csv = new Tabular.Csv() { Data = x.Content };
+                    Tabular.Table table = Tabular.Csv.FromCsv(csv);
+                    StringBuilder builder = new StringBuilder();
+
+                    bool firstLine = true;
+                    builder.AppendLine("<table>");
+                    foreach (var row in table.Rows)
+                    {
+                        builder.AppendLine("<tr>");
+                        foreach (var cell in row)
+                        {
+                            if (_firstLineHeader && firstLine)
+                                builder.AppendLine($"<th>{cell.Value}</th>");
+                            else
+                                builder.AppendLine($"<td>{cell.Value}</td>");
+                        }
+                        builder.AppendLine("</tr>");
+                        firstLine = false;
+                    }
+                    builder.AppendLine("</table>");
+                    return x.Clone(builder.ToString());
+
+                }
+                catch (Exception e)
+                {
+                    context.Trace.Error($"An {e.ToString()} occured ({x.Source}).\n\t{e.Message}");
+                    return null;
+                }
+            }).Where(x => x != null);
+        }
+    }
+}
---
+++ Wyam.Modules.Tables/ExcelToCsv.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common.Configuration;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+
+namespace Wyam.Modules.Tables
+{
+    public class ExcelToCsv : IModule
+    {
+
+        private DocumentConfig _isXslx;
+
+        public ExcelToCsv(bool isXslx = true)
+        {
+            _isXslx = (a, b) => isXslx;
+        }
+        public ExcelToCsv(DocumentConfig isXslx)
+        {
+            _isXslx = isXslx;
+        }
+
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            return inputs.AsParallel().Select(x =>
+            {
+                try
+                {
+                    using (var stream = x.GetStream())
+                    {
+                        Tabular.Table table = Tabular.Excel.ReadFrom(stream, _isXslx.Invoke<bool>(x, context) ? Tabular.ExcelFormat.Excel2007 : Tabular.ExcelFormat.Excel2003);
+                        Tabular.Csv csv = Tabular.Csv.ToCsv(table);
+                        return x.Clone(csv.Data);
+                    }
+                }
+                catch (Exception e)
+                {
+                    context.Trace.Error($"An {e.ToString()} occured ({x.Source}).\n\t{e.Message}");
+                    return null;
+                }
+            }).Where(x => x != null);
+        }
+    }
+}
