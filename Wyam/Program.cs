3626d342e55c760d3f8389c17c0dddc29423bbae
Dave Glick <dave@daveaglick.com>
2015-12-30 18:10:07 -0500

    Adds support for serving unknown file types to preview server, resolves #175

--- Wyam/Program.cs
+++ Wyam/Program.cs
@@ -400,6 +400,7 @@ namespace Wyam
                 {
                     RequestPath = PathString.Empty,
                     FileSystem = outputFolder,
+                    ServeUnknownFileTypes = true
                 });
             });
         }
