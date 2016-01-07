11a9f00caa1a7ba3c11fab2f236216868eb33bed
Dave Glick <dave@daveaglick.com>
2016-01-07 13:53:11 -0500

    Adds ability to render Markdown content stored in metadata

--- Wyam.Modules.Markdown.Tests/MarkdownFixture.cs
+++ Wyam.Modules.Markdown.Tests/MarkdownFixture.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using NSubstitute;
 using System.IO;
 using Wyam.Common.Documents;
+using Wyam.Common.Meta;
 using Wyam.Common.Pipelines;
 
 namespace Wyam.Modules.Markdown.Tests
@@ -80,6 +81,85 @@ namespace Wyam.Modules.Markdown.Tests
             // Then
             document.Received(1).Clone(Arg.Any<string>());
             document.Received().Clone(output);
+        }
+
+        [Test]
+        public void RendersMarkdownFromMetadata()
+        {
+            // Given
+            string input = @"Line 1
+*Line 2*
+# Line 3";
+            string output = @"<p>Line 1
+<em>Line 2</em></p>
+<h1>Line 3</h1>
+
+";
+
+            IDocument document = Substitute.For<IDocument>();
+            document.ContainsKey("meta").Returns(true);
+            document.String("meta").Returns(input);
+            IExecutionContext context = Substitute.For<IExecutionContext>();
+            Markdown markdown = new Markdown("meta");
+
+            // When
+            markdown.Execute(new[] { document }, context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            document.Received(0).Clone(Arg.Any<string>());
+            document.Received(1).Clone(Arg.Any<MetadataItems>());
+            document.Received().Clone(Arg.Is<MetadataItems>(x => x.SequenceEqual(new MetadataItems
+            {
+                { "meta", output }
+            })));
+        }
+
+        [Test]
+        public void RendersMarkdownFromMetadataToNewKey()
+        {
+            // Given
+            string input = @"Line 1
+*Line 2*
+# Line 3";
+            string output = @"<p>Line 1
+<em>Line 2</em></p>
+<h1>Line 3</h1>
+
+";
+
+            IDocument document = Substitute.For<IDocument>();
+            document.ContainsKey("meta").Returns(true);
+            document.String("meta").Returns(input);
+            IExecutionContext context = Substitute.For<IExecutionContext>();
+            Markdown markdown = new Markdown("meta", "meta2");
+
+            // When
+            markdown.Execute(new[] { document }, context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            document.Received(0).Clone(Arg.Any<string>());
+            document.Received(1).Clone(Arg.Any<MetadataItems>());
+            document.Received().Clone(Arg.Is<MetadataItems>(x => x.SequenceEqual(new MetadataItems
+            {
+                { "meta2", output }
+            })));
+        }
+
+        [Test]
+        public void DoesNothingIfMetadataKeyDoesNotExist()
+        {
+            // Given
+            IDocument document = Substitute.For<IDocument>();
+            document.ContainsKey("meta").Returns(false);
+            IExecutionContext context = Substitute.For<IExecutionContext>();
+            Markdown markdown = new Markdown("meta");
+
+            // When
+            markdown.Execute(new[] { document }, context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            document.Received(0).Clone(Arg.Any<string>());
+            document.Received(0).Clone(Arg.Any<MetadataItems>());
         }
     }
 }
--- Wyam.Modules.Markdown/Markdown.cs
+++ Wyam.Modules.Markdown/Markdown.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Wyam.Common;
 using Wyam.Common.Caching;
 using Wyam.Common.Documents;
+using Wyam.Common.Meta;
 using Wyam.Common.Modules;
 using Wyam.Common.Pipelines;
 
@@ -20,7 +21,28 @@ namespace Wyam.Modules.Markdown
     /// <category>Templates</category>
     public class Markdown : IModule
     {
+        private readonly string _sourceKey;
+        private readonly string _destinationKey;
         private bool _escapeAt = true;
+
+
+        /// <summary>
+        /// Processes Markdown in the content of the document.
+        /// </summary>
+        public Markdown()
+        {
+        }
+
+        /// <summary>
+        /// Processes Markdown in the metadata of the document. The rendered HTML will be placed
+        /// </summary>
+        /// <param name="sourceKey">The metadata key of the Markdown to process.</param>
+        /// <param name="destinationKey">The metadata key to store the rendered HTML (if null, it gets placed back in the source metadata key).</param>
+        public Markdown(string sourceKey, string destinationKey = null)
+        {
+            _sourceKey = sourceKey;
+            _destinationKey = destinationKey;
+        }
 
         /// <summary>
         /// Specifies whether the <c>@</c> symbol should be escaped (the default is <c>true</c>).
@@ -39,19 +61,37 @@ namespace Wyam.Modules.Markdown
         {
             return inputs.AsParallel().Select(x =>
             {
-                context.Trace.Verbose("Processing Markdown for {0}", x.Source);
+                context.Trace.Verbose("Processing Markdown {0}for {1}", 
+                    string.IsNullOrEmpty(_sourceKey) ? string.Empty : ("in" + _sourceKey), x.Source);
                 string result;
                 IExecutionCache executionCache = context.ExecutionCache;
-                if (!executionCache.TryGetValue<string>(x, out result))
+                if (!executionCache.TryGetValue<string>(x, _sourceKey, out result))
                 {
-                    result = CommonMark.CommonMarkConverter.Convert(x.Content);
+                    if (string.IsNullOrEmpty(_sourceKey))
+                    {
+                        result = CommonMark.CommonMarkConverter.Convert(x.Content);
+                    }
+                    else
+                    {
+                        if (!x.ContainsKey(_sourceKey))
+                        {
+                            // Don't do anything if the key doesn't exist
+                            return x;
+                        }
+                        result = CommonMark.CommonMarkConverter.Convert(x.String(_sourceKey) ?? string.Empty);
+                    }
                     if (_escapeAt)
                     {
                         result = result.Replace("@", "&#64;");
                     }
-                    executionCache.Set(x, result);
+                    executionCache.Set(x, _sourceKey, result);
                 }
-                return x.Clone(result);
+                return string.IsNullOrEmpty(_sourceKey)
+                    ? x.Clone(result)
+                    : x.Clone(new MetadataItems
+                    {
+                        { string.IsNullOrEmpty(_destinationKey) ? _sourceKey : _destinationKey, result }
+                    });
             });
         }
     }
