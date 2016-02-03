a2da496cea092a00291f8c5398260c8bf059d024
Dave Glick <dave@daveaglick.com>
2016-02-03 15:44:03 -0500

    Resetting SolutionInfo - should not have been incremented yet

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Wyam")]
-[assembly: AssemblyVersion("0.11.2")]
-[assembly: AssemblyFileVersion("0.11.2")]
-[assembly: AssemblyInformationalVersion("0.11.2-beta")]
+[assembly: AssemblyVersion("0.11.1")]
+[assembly: AssemblyFileVersion("0.11.1")]
+[assembly: AssemblyInformationalVersion("0.11.1-beta")]
 [assembly: AssemblyCopyright("Copyright (c) Wyam Contributors")]
 
