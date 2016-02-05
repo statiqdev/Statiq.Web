cf25f7de140c11425e1caf6a541155a07bb61c14
Deane Barker <Deane Barker>
2016-02-05 17:31:14 -0600

    Added a Take module.

---
+++ src/Wyam.Core/Modules/Control/Take.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using Wyam.Common.Documents;
+using Wyam.Common.Modules;
+using Wyam.Common.Pipelines;
+
+namespace Wyam.Core.Modules.Control
+{
+    /// <summary>
+    /// Takes the first X documents from the current pipeline and discards the rest.
+    /// </summary>
+    /// <category>Control</category>
+    public class Take : IModule
+    {
+        private readonly int _x;
+
+        /// <summary>
+        /// Takes the first X documents from the current pipeline and discards the rest.
+        /// </summary>
+        /// <param name="x">An integer represeting the number of documents to preserve from the current pipeline.</param>
+        public Take(int x)
+        {
+            _x = x;
+        }
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            return inputs.Take(_x);
+        }
+    }
+}
