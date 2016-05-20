31b270d9314ebc7e84d2b5b06baa2e261c08e86f
Dave Glick <dave@daveaglick.com>
2016-05-20 10:34:39 -0400

    Adding comments for IMetadataValue and related helpers

--- src/Wyam.Common/Meta/CachedDelegateMetadataValue.cs
+++ src/Wyam.Common/Meta/CachedDelegateMetadataValue.cs
@@ -3,18 +3,35 @@ using Wyam.Common.Documents;
 
 namespace Wyam.Common.Meta
 {
-    // This uses a delegate to get the metadata value and caches the result
-    // This results in a tradeoff between memory consumption and performance (both potentially greater if the value cached)
-    // Note that the provided delegate should be thread-safe
+    /// <summary>
+    /// This class uses a delegate to get a metadata value. The result of the delegate
+    /// will be cached and the cached value will be returned for subsequent calls to <see cref="Get"/>.
+    /// </summary>
     public class CachedDelegateMetadataValue : DelegateMetadataValue
     {
         private object _cachedValue;
         private bool _cached;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachedDelegateMetadataValue"/> class.
+        /// The specified delegate should be thread-safe.
+        /// </summary>
+        /// <param name="value">The delegate that returns the metadata value.</param>
         public CachedDelegateMetadataValue(Func<string, IMetadata, object> value) : base(value)
         {
         }
 
+        /// <summary>
+        /// Gets the value for the specified key. This method will be called
+        /// for each request of this metadata value and the return object will
+        /// be processed like any other metadata value. The implementation
+        /// of this method must be thread-safe.
+        /// </summary>
+        /// <param name="key">The metadata key.</param>
+        /// <param name="metadata">The metadata value.</param>
+        /// <returns>
+        /// The object to use as the value for the requested key.
+        /// </returns>
         public override object Get(string key, IMetadata metadata)
         {
             if (!_cached)
--- src/Wyam.Common/Meta/IMetadataValue.cs
+++ src/Wyam.Common/Meta/IMetadataValue.cs
@@ -1,10 +1,19 @@
 ﻿namespace Wyam.Common.Meta
 {
-    // Implement this interface to provide lazy metadata values or values based on other metadata
-    // The Get(...) method will be called for each request of this value and then processed like any other value
+    /// <summary>
+    /// Implement this interface to provide lazy metadata values or values based on other metadata.
+    /// </summary>
     public interface IMetadataValue
     {
-        // The implementation of this method must be thread-safe
+        /// <summary>
+        /// Gets the value for the specified key. This method will be called 
+        /// for each request of this metadata value and the return object will
+        /// be processed like any other metadata value. The implementation 
+        /// of this method must be thread-safe.
+        /// </summary>
+        /// <param name="key">The metadata key.</param>
+        /// <param name="metadata">The metadata value.</param>
+        /// <returns>The object to use as the value for the requested key.</returns>
         object Get(string key, IMetadata metadata);
     }
 }
