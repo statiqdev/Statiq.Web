78c6048fd36a99e95d375a24412760a82d5734f3
Patrick Kranz <patrick-kranz@live.de>
2016-05-31 14:35:40 +0200

    added Sidescarfile Module, but tests still need to be implemented.

---
+++ src/Wyam.Core.Tests/Modules/Control/SidecarTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Core.Documents;
+using Wyam.Core.Modules.Control;
+using Wyam.Core.Modules.Extensibility;
+using Wyam.Core.Execution;
+using Wyam.Testing;
+using Wyam.Common.IO;
+
+namespace Wyam.Core.Tests.Modules.Control
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self | ParallelScope.Children)]
+    public class SideCarTests : BaseFixture
+    {
+        public class ExecuteMethodTests : FrontMatterTests
+        {
+            [Test]
+            public void LoadsSidecarFile()
+            {
+                // Given
+                Engine engine = new Engine();
+                Pipeline pipeline = new Pipeline("Pipeline", null);
+                IExecutionContext context = new ExecutionContext(engine, pipeline);
+
+                string documentContent = "I'm document content";
+                string sidecarContent = "I'm sidecar content";
+
+                string documentPath = "/test.md";
+                string sidecarPath = "/test.md.meta";
+
+
+                IDocument[] inputs =
+                {
+                    context.GetDocument((FilePath)documentPath, "")
+                };
+                string lodedSidecarContent = null;
+                Sidecar sidecar = new Sidecar(new Execute((x, ctx) =>
+                {
+                    lodedSidecarContent = x.Content;
+                    return new[] { x };
+                }));
+
+                // When
+                IEnumerable<IDocument> documents = sidecar.Execute(inputs, context);
+
+                // Then
+
+                Assert.AreEqual(sidecarContent, lodedSidecarContent);
+                Assert.AreEqual(documentContent, documents.Single().Content);
+
+
+            }
+
+        }
+    }
+}
