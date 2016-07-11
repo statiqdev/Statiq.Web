0b975547e08be4f3339f990c0c121f08b5031be9
Dave Glick <dave@daveaglick.com>
2016-07-11 14:40:30 -0400

    Final prep work for 0.13.0 release

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Wyam")]
-[assembly: AssemblyVersion("0.12.4")]
-[assembly: AssemblyFileVersion("0.12.4")]
-[assembly: AssemblyInformationalVersion("0.12.4-beta")]
+[assembly: AssemblyVersion("0.13.0")]
+[assembly: AssemblyFileVersion("0.13.0")]
+[assembly: AssemblyInformationalVersion("0.13.0-beta")]
 [assembly: AssemblyCopyright("Copyright © Wyam Contributors")]
 
