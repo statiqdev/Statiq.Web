351159ecd9b3e1a20f08c6dbbbc35ffb2308196a
Dave Glick <dave@daveaglick.com>
2016-09-01 12:15:19 -0400

    Initial ASP.NET MVC example w/o Wyam

---
+++ examples/AspNetMvc/AspNetMvc/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
