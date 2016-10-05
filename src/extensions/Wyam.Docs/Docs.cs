9b1f2d03c5e6f51a17feff0d4ca319e391d92085
Dave Glick <dave@daveaglick.com>
2016-10-05 18:06:37 -0400

    Initial skeleton project for docs recipe, #342

---
+++ src/extensions/Wyam.Docs/Docs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.CodeAnalysis;
+using Wyam.Common.Configuration;
+using Wyam.Common.Execution;
+using Wyam.Common.IO;
+using Wyam.Core.Modules.Control;
+using Wyam.Core.Modules.Extensibility;
+using Wyam.Core.Modules.IO;
+using Wyam.Razor;
+
+namespace Wyam.Docs
+{
+    public class Docs : IRecipe
+    {
+        public void Apply(IEngine engine)
+        {
+            // Global metadata defaults
+            engine.GlobalMetadata[DocsKeys.SourceFiles] = "src/**/*.cs";
+            engine.GlobalMetadata[DocsKeys.IncludeGlobal] = true;
+            engine.GlobalMetadata[DocsKeys.ApiPathPrefix] = "api";
+
+            engine.Pipelines.Add(DocsPipelines.Code,
+                new ReadFiles((doc, ctx) => ctx.GlobalMetadata.List<string>(DocsKeys.SourceFiles))
+            );
+
+            engine.Pipelines.Add(DocsPipelines.Api,
+                new Documents(DocsPipelines.Code),
+                // Put analysis module inside execute to have access to global metadata at runtime
+                new Execute(ctx => new AnalyzeCSharp()
+                    .WhereNamespaces(ctx.GlobalMetadata.Get<bool>(DocsKeys.IncludeGlobal))
+                    .WherePublic()
+                    .WithCssClasses("pre", "prettyprint")
+                    .WithWritePathPrefix(ctx.GlobalMetadata.String(DocsKeys.ApiPathPrefix))),
+                new Razor.Razor()
+                    .WithLayout("_ApiLayout.cshtml"),
+                new WriteFiles()
+            );
+        }
+
+        public void Scaffold(IDirectory directory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/extensions/Wyam.Docs/DocsKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyam.Docs
+{
+    public static class DocsKeys
+    {
+        public const string SourceFiles = nameof(SourceFiles);
+        public const string IncludeGlobal = nameof(IncludeGlobal);
+        public const string ApiPathPrefix = nameof(ApiPathPrefix);
+    }
+}
