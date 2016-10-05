24e61a821a04e988f1abe9d794cdf61181551438
Dave Glick <dave@daveaglick.com>
2016-10-05 18:06:37 -0400

    Initial skeleton project for docs recipe, #342

---
+++ src/extensions/Wyam.Docs/DocsPipelines.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyam.Docs
+{
+    public static class DocsPipelines
+    {
+        public const string Code = nameof(Code);
+        public const string Api = nameof(Api);
+    }
+}
---
+++ src/extensions/Wyam.Docs/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Wyam.Docs")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0eb71e59-8d5c-45f3-bf1d-8649ba76d139")]
