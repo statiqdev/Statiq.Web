1db4188258ca30790022e085a145d020918613be
Dave Glick <dave@daveaglick.com>
2016-10-07 18:07:57 -0400

    Theme updates for side nav and wrapping of type and member names, #342

--- src/extensions/Wyam.Docs/Extensions.cs
+++ src/extensions/Wyam.Docs/Extensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Html;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Html;
 using Wyam.Common.Documents;
 using Wyam.Common.Execution;
 using Wyam.Common.Meta;
@@ -13,14 +17,35 @@ namespace Wyam.Docs
         public static HtmlString Name(this IMetadata metadata)
         {
             string name = metadata.String("DisplayName");
-            return name == null
-                ? new HtmlString(string.Empty)
-                : new HtmlString(System.Net.WebUtility.HtmlEncode(name.ToString()).ToString()
-                    .Replace(".", "<wbr>.")
-                    .Replace("(", "<wbr>(")
-                    .Replace(")", ")<wbr>")
-                    .Replace("&lt;", "<wbr>&lt;")
-                    .Replace("&gt;", "&gt;<wbr>"));
+            if (name == null)
+            {
+                return new HtmlString(string.Empty);
+            }
+
+            // Encode and replace .()<> with word break opportunities
+            name = System.Net.WebUtility.HtmlEncode(name)
+                .Replace(".", "<wbr>.")
+                .Replace("(", "<wbr>(")
+                .Replace(")", ")<wbr>")
+                .Replace(", ", ", <wbr>")
+                .Replace("&lt;", "<wbr>&lt;")
+                .Replace("&gt;", "&gt;<wbr>");
+
+            // Add additional break opportunities in long un-broken segments
+            List<string> segments = name.Split(new[] { "<wbr>" }, StringSplitOptions.None).ToList();
+            bool replaced = false;
+            for (int c = 0; c < segments.Count; c++)
+            {
+                if (segments[c].Length > 20)
+                {
+                    segments[c] = new string(segments[c]
+                        .SelectMany((x, i) => char.IsUpper(x) && i != 0 ? new[] { '<', 'w', 'b', 'r', '>', x } : new[] { x })
+                        .ToArray());
+                    replaced = true;
+                }
+            }
+
+            return new HtmlString(replaced ? string.Join("<wbr>", segments) : name);
         }
 
         public static HtmlString GetTypeLink(this IExecutionContext context, IMetadata metadata)
