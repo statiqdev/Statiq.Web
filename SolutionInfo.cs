55214eac0f31dbbe3009fc41889721e5d91abeb6
Dave Glick <dave@daveaglick.com>
2016-10-31 11:33:34 -0400

    Updating version and release notes

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Wyam")]
-[assembly: AssemblyVersion("0.14.1")]
-[assembly: AssemblyFileVersion("0.14.1")]
-[assembly: AssemblyInformationalVersion("0.14.1-beta")]
+[assembly: AssemblyVersion("0.14.2")]
+[assembly: AssemblyFileVersion("0.14.2")]
+[assembly: AssemblyInformationalVersion("0.14.2-beta")]
 [assembly: AssemblyCopyright("Copyright © Wyam Contributors")]
 
