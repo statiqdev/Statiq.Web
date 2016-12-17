e4f870b70c23da20d11b281b7d64a95f8ba0ee1e
Dave Glick <dave@daveaglick.com>
2016-12-16 21:29:25 -0500

    Adding comments back to AnalyzeCSharp

--- src/extensions/Wyam.CodeAnalysis/AnalyzeCSharp.cs
+++ src/extensions/Wyam.CodeAnalysis/AnalyzeCSharp.cs
@@ -18,6 +18,20 @@ using Wyam.Common.Tracing;
 
 namespace Wyam.CodeAnalysis
 {
+    /// <summary>
+    /// Performs static code analysis on the input documents, outputting a new document for each symbol.
+    /// </summary>
+    /// <remarks>
+    /// This module acts as the basis for code analysis scenarios such as generating source code documentation.
+    /// All input documents are assumed to contain C# source in their content and are used to create a Roslyn
+    /// compilation. All symbols (namespaces, types, members, etc.) in the compilation are then recursively 
+    /// processed and output from this module as documents, one per symbol. The output documents have empty content
+    /// and all information about the symbol is contained in the metadata. This lets you pass the output documents
+    /// for each symbol on to a template engine like Razor and generate pages for each symbol by having the
+    /// template use the document metadata.
+    /// </remarks>
+    /// <include file="Documentation.xml" path="/Documentation/AnalyzeCSharp/*" />
+    /// <category>Metadata</category>
     public class AnalyzeCSharp : IModule
     {
         internal const string CompilationAssemblyName = nameof(CompilationAssemblyName);
