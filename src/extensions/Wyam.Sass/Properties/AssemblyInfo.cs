1a9c371910a57c5f492cf749703f494e17ab8ded
Phil <phil.scott@gmail.com>
2017-02-07 17:31:22 -0500

    Adds Sass template processing
    This more or less copies the LESS module but adds Sass processing using
    LibSass.Net. LibSass.Net is a wrapper around the C library LibSass which
    worried me a bit, but everything seems to wire up just fine.
    
    The test project is pretty simplistic. I would have liked to have a few
    test cases for testing out that I got the include functionality right, but
    I couldn't wrap my head around how to do that easily. But basic
    verification of a successful call and a failing call are there for now.

---
+++ src/extensions/Wyam.Sass/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wyam.Sass")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("90a0ab19-0146-4931-befa-d895b192e050")]
---
+++ tests/extensions/Wyam.Sass.Tests/SassFixture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Testing;
+
+namespace Wyam.Sass.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self | ParallelScope.Children)]
+    public class SassFixture : BaseFixture
+    {
+        [Test]
+        public void Convert()
+        {
+            string input = @"
+$font-stack:    Helvetica, sans-serif;
+$primary-color: #333;
+
+body {
+  font: 100% $font-stack;
+  color: $primary-color;
+}";
+
+            string output = "body { font: 100% Helvetica, sans-serif; color: #333; }\n";
+
+            IExecutionContext context = Substitute.For<IExecutionContext>();
+            IDocument document = Substitute.For<IDocument>();
+            document.Content.Returns(input);
+
+            Sass sass = new Sass().IncludeSourceComments(false).WithCompactOutputStyle();
+
+            // When
+            sass.Execute(new[] { document }, context).ToList();  // Make sure to materialize the result list
+
+            // Then
+            context.Received(1).GetDocument(Arg.Any<IDocument>(), Arg.Any<string>());
+            context.Received().GetDocument(document, output);
+        }
+
+        [Test]
+        public void ConvertingBadSassFails()
+        {
+            string input = @"
+$font-stack:    Helvetica, sans-serif
+$primary-color: #333
+
+body {
+  font: 100% $font-stack;
+  color: $primary-color;
+}";
+
+            IExecutionContext context = Substitute.For<IExecutionContext>();
+            IDocument document = Substitute.For<IDocument>();
+            document.Content.Returns(input);
+
+            Sass sass = new Sass();
+
+            // That
+            Assert.Catch<AggregateException>(() => 
+            {
+                sass.Execute(new[] { document }, context).ToList();  // Make sure to materialize the result list
+            });
+        }
+    }
+}
