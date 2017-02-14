e92593dd600fa7d4d5d89166f8efb8eefb2f348f
Mark Lopez <m@silvenga.com>
2017-02-13 21:52:57 -0600

    Separated Owin configuration.

--- src/clients/Wyam/PreviewServer.cs
+++ src/clients/Wyam/PreviewServer.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+
 using Microsoft.Owin;
 using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.Hosting;
 using Microsoft.Owin.Hosting.Tracing;
 using Microsoft.Owin.StaticFiles;
+
 using Owin;
+
 using Wyam.Common.IO;
 using Wyam.Common.Tracing;
 using Wyam.Owin;
@@ -26,48 +29,7 @@ namespace Wyam
                 // http://stackoverflow.com/questions/17948363/tracelistener-in-owin-self-hosting
                 options.Settings.Add(typeof(ITraceOutputFactory).FullName, typeof(NullTraceOutputFactory).AssemblyQualifiedName);
 
-                server = WebApp.Start(options, app =>
-                {
-                    Microsoft.Owin.FileSystems.IFileSystem outputFolder = new PhysicalFileSystem(path.FullPath);
-
-                    // Support for virtual directory
-                    if (virtualDirectory != null)
-                    {
-                        app.UseVirtualDirectory(virtualDirectory.FullPath);
-                    }
-
-                    // Disable caching
-                    app.Use((c, t) =>
-                    {
-                        c.Response.Headers.Append("Cache-Control", "no-cache, no-store, must-revalidate");
-                        c.Response.Headers.Append("Pragma", "no-cache");
-                        c.Response.Headers.Append("Expires", "0");
-                        return t();
-                    });
-
-                    // Support for extensionless URLs
-                    if (!forceExtension)
-                    {
-                        app.UseExtensionlessUrls(new ExtensionlessUrlsOptions
-                        {
-                            FileSystem = outputFolder
-                        });
-                    }
-
-                    // Serve up all static files
-                    app.UseDefaultFiles(new DefaultFilesOptions
-                    {
-                        RequestPath = PathString.Empty,
-                        FileSystem = outputFolder,
-                        DefaultFileNames = new List<string> { "index.html", "index.htm", "home.html", "home.htm", "default.html", "default.html" }
-                    });
-                    app.UseStaticFiles(new StaticFileOptions
-                    {
-                        RequestPath = PathString.Empty,
-                        FileSystem = outputFolder,
-                        ServeUnknownFileTypes = true
-                    });
-                });
+                server = WebApp.Start(options, app => ConfigureOwin(app, path.Name, forceExtension, virtualDirectory));
             }
             catch (Exception ex)
             {
@@ -76,8 +38,51 @@ namespace Wyam
             }
 
             Trace.Information($"Preview server listening on port {port} and serving from path {path}"
-                + (virtualDirectory == null ? string.Empty : $" with virtual directory {virtualDirectory.FullPath}"));
+                              + (virtualDirectory == null ? string.Empty : $" with virtual directory {virtualDirectory.FullPath}"));
             return server;
+        }
+
+        internal static void ConfigureOwin(IAppBuilder app, DirectoryPath path, bool forceExtension, DirectoryPath virtualDirectory)
+        {
+            Microsoft.Owin.FileSystems.IFileSystem outputFolder = new PhysicalFileSystem(path.FullPath);
+
+            // Support for virtual directory
+            if (virtualDirectory != null)
+            {
+                app.UseVirtualDirectory(virtualDirectory.FullPath);
+            }
+
+            // Disable caching
+            app.Use((c, t) =>
+            {
+                c.Response.Headers.Append("Cache-Control", "no-cache, no-store, must-revalidate");
+                c.Response.Headers.Append("Pragma", "no-cache");
+                c.Response.Headers.Append("Expires", "0");
+                return t();
+            });
+
+            // Support for extensionless URLs
+            if (!forceExtension)
+            {
+                app.UseExtensionlessUrls(new ExtensionlessUrlsOptions
+                {
+                    FileSystem = outputFolder
+                });
+            }
+
+            // Serve up all static files
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = outputFolder,
+                DefaultFileNames = new List<string> {"index.html", "index.htm", "home.html", "home.htm", "default.html", "default.html"}
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = outputFolder,
+                ServeUnknownFileTypes = true
+            });
         }
 
         private class NullTraceOutputFactory : ITraceOutputFactory
