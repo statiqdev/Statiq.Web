9649ae397a5b22915d2444ad8dd9bf730e710e12
Mark Lopez <m@silvenga.com>
2017-02-14 20:59:44 -0600

    Moves Preview Server to Use Kestrel (#449)
    * Added Kestrel in Win-x86 mode.
    
    * Separated Owin configuration.
    
    * Moved PreviewServer to use Kestrel.
    
    * Removed now unused packages.

--- src/clients/Wyam/PreviewServer.cs
+++ src/clients/Wyam/PreviewServer.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
+
 using Microsoft.Owin;
 using Microsoft.Owin.FileSystems;
-using Microsoft.Owin.Hosting;
-using Microsoft.Owin.Hosting.Tracing;
 using Microsoft.Owin.StaticFiles;
+
 using Owin;
+
 using Wyam.Common.IO;
 using Wyam.Common.Tracing;
 using Wyam.Owin;
+using Wyam.Server;
 
 namespace Wyam
 {
@@ -17,57 +18,11 @@ namespace Wyam
     {
         public static IDisposable Start(DirectoryPath path, int port, bool forceExtension, DirectoryPath virtualDirectory)
         {
-            IDisposable server;
+            HttpServer server;
             try
             {
-                StartOptions options = new StartOptions("http://localhost:" + port);
-
-                // Disable built-in owin tracing by using a null trace output
-                // http://stackoverflow.com/questions/17948363/tracelistener-in-owin-self-hosting
-                options.Settings.Add(typeof(ITraceOutputFactory).FullName, typeof(NullTraceOutputFactory).AssemblyQualifiedName);
-
-                server = WebApp.Start(options, app =>
-                {
-                    Microsoft.Owin.FileSystems.IFileSystem outputFolder = new PhysicalFileSystem(path.FullPath);
-
-                    // Support for virtual directory
-                    if (virtualDirectory != null)
-                    {
-                        app.UseVirtualDirectory(virtualDirectory.FullPath);
-                    }
-
-                    // Disable caching
-                    app.Use((c, t) =>
-                    {
-                        c.Response.Headers.Append("Cache-Control", "no-cache, no-store, must-revalidate");
-                        c.Response.Headers.Append("Pragma", "no-cache");
-                        c.Response.Headers.Append("Expires", "0");
-                        return t();
-                    });
-
-                    // Support for extensionless URLs
-                    if (!forceExtension)
-                    {
-                        app.UseExtensionlessUrls(new ExtensionlessUrlsOptions
-                        {
-                            FileSystem = outputFolder
-                        });
-                    }
-
-                    // Serve up all static files
-                    app.UseDefaultFiles(new DefaultFilesOptions
-                    {
-                        RequestPath = PathString.Empty,
-                        FileSystem = outputFolder,
-                        DefaultFileNames = new List<string> { "index.html", "index.htm", "home.html", "home.htm", "default.html", "default.html" }
-                    });
-                    app.UseStaticFiles(new StaticFileOptions
-                    {
-                        RequestPath = PathString.Empty,
-                        FileSystem = outputFolder,
-                        ServeUnknownFileTypes = true
-                    });
-                });
+                server = new HttpServer();
+                server.StartServer(port, app => ConfigureOwin(app, path, forceExtension, virtualDirectory));
             }
             catch (Exception ex)
             {
@@ -76,16 +31,51 @@ namespace Wyam
             }
 
             Trace.Information($"Preview server listening on port {port} and serving from path {path}"
-                + (virtualDirectory == null ? string.Empty : $" with virtual directory {virtualDirectory.FullPath}"));
+                              + (virtualDirectory == null ? string.Empty : $" with virtual directory {virtualDirectory.FullPath}"));
             return server;
         }
 
-        private class NullTraceOutputFactory : ITraceOutputFactory
+        internal static void ConfigureOwin(IAppBuilder app, DirectoryPath path, bool forceExtension, DirectoryPath virtualDirectory)
         {
-            public TextWriter Create(string outputFile)
+            Microsoft.Owin.FileSystems.IFileSystem outputFolder = new PhysicalFileSystem(path.FullPath);
+
+            // Support for virtual directory
+            if (virtualDirectory != null)
             {
-                return StreamWriter.Null;
+                app.UseVirtualDirectory(virtualDirectory.FullPath);
             }
+
+            // Disable caching
+            app.Use((c, t) =>
+            {
+                c.Response.Headers.Append("Cache-Control", "no-cache, no-store, must-revalidate");
+                c.Response.Headers.Append("Pragma", "no-cache");
+                c.Response.Headers.Append("Expires", "0");
+                return t();
+            });
+
+            // Support for extensionless URLs
+            if (!forceExtension)
+            {
+                app.UseExtensionlessUrls(new ExtensionlessUrlsOptions
+                {
+                    FileSystem = outputFolder
+                });
+            }
+
+            // Serve up all static files
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = outputFolder,
+                DefaultFileNames = new List<string> {"index.html", "index.htm", "home.html", "home.htm", "default.html", "default.html"}
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = outputFolder,
+                ServeUnknownFileTypes = true
+            });
         }
     }
 }
---
+++ src/clients/Wyam/Server/HttpServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+using Owin;
+
+namespace Wyam.Server
+{
+    internal class HttpServer : IDisposable
+    {
+        private IWebHost _host;
+
+        public void StartServer(int port, Action<IAppBuilder> owinConfiguration)
+        {
+            _host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://localhost:{port}")
+                .Configure(builder =>
+                {
+                    // Enable websocket support
+                    builder.UseWebSockets();
+
+                    // Support drop-in replacement
+                    builder.UseOwinBuilder(owinConfiguration);
+                })
+                .Build();
+            _host.Start();
+        }
+
+        public void Dispose()
+        {
+            _host?.Dispose();
+        }
+    }
+}
