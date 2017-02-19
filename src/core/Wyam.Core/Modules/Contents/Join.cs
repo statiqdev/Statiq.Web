72a5c17668128c5cedf91d02fdfa82c59044be53
James Flanagan <bob_ganges@hotmail.com>
2017-02-19 08:57:11 +0000

    Join now works with multiple files and no delimator - currently returns default metadata only

---
+++ src/core/Wyam.Core/Modules/Contents/Join.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Common.Modules;
+
+namespace Wyam.Core.Modules.Contents
+{
+    /// <summary>
+    /// Joins documents together with an optional deliminator to form one document
+    /// </summary>
+    /// <category>Content</category>
+    public class Join : IModule
+    {
+
+        /// <summary>
+        /// Returns a single document containing the concatenated content of all input documents
+        /// </summary>
+        /// <returns>A single document in a list</returns>
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            StringBuilder contentBuilder = new StringBuilder();
+
+            if (inputs == null)
+            {
+                return new List<IDocument>() { context.GetDocument() };
+            }
+
+            foreach(var document in inputs)
+            {
+                if (document == null) continue;
+
+                contentBuilder.Append(document.Content);
+            }
+            
+            return new List<IDocument>() { context.GetDocument(contentBuilder.ToString(), new KeyValuePair<string, object>[0]) };
+        }
+    }
+
+    //TODO - update comments - meta data and delimator
+}
---
+++ tests/core/Wyam.Core.Tests/Modules/Contents/JoinFixture.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Common.IO;
+using Wyam.Common.Meta;
+using Wyam.Core.Modules.Contents;
+using Wyam.Testing;
+using Wyam.Testing.Documents;
+using Wyam.Testing.Execution;
+
+namespace Wyam.Core.Tests.Modules.Contents
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self | ParallelScope.Children)]
+    public class JoinFixture : BaseFixture
+    {
+        [Test]
+        public void TwoDocumentsJoinWithNoDelimiter()
+        {
+            // Given
+            IDocument first = new TestDocument("Test");
+            IDocument second = new TestDocument("Test2");
+
+            IExecutionContext context = new TestExecutionContext();
+            Join join = new Join();
+
+            // When
+            List<IDocument> results = join.Execute(new[] { first, second }, context).ToList();
+
+            // Then
+            Assert.AreEqual("TestTest2", results.Single().Content);
+        }
+
+        [Test]
+        public void ThreeDocumentsJoinWithNoDelimiter()
+        {
+            // Given
+            IDocument first = new TestDocument("Test");
+            IDocument second = new TestDocument("Test2");
+            IDocument third = new TestDocument("Test3");
+
+            IExecutionContext context = new TestExecutionContext();
+            Join join = new Join();
+
+            // When
+            List<IDocument> results = join.Execute(new[] { first, second, third }, context).ToList();
+
+            // Then
+            Assert.AreEqual("TestTest2Test3", results.Single().Content);
+        }
+
+        [Test]
+        public void ThreeDocumentsJoinWithNoDelimiter_firstnull()
+        {
+            // Given
+            IDocument first = null;
+            IDocument second = new TestDocument("Test2");
+            IDocument third = new TestDocument("Test3");
+
+            IExecutionContext context = new TestExecutionContext();
+            Join join = new Join();
+
+            // When
+            List<IDocument> results = join.Execute(new[] { first, second, third }, context).ToList();
+
+            // Then
+            Assert.AreEqual("Test2Test3", results.Single().Content);
+        }
+
+        [Test]
+        public void ThreeDocumentsJoinWithNoDelimiter_secondnull()
+        {
+            // Given
+            IDocument first = new TestDocument("Test");
+            IDocument second = null;
+            IDocument third = new TestDocument("Test3");
+
+            IExecutionContext context = new TestExecutionContext();
+            Join join = new Join();
+
+            // When
+            List<IDocument> results = join.Execute(new[] { first, second, third }, context).ToList();
+
+            // Then
+            Assert.AreEqual("TestTest3", results.Single().Content);
+        }
+
+
+        [Test]
+        public void nullPassedInAsDocumentList()
+        {
+            // Given
+            IExecutionContext context = new TestExecutionContext();
+            Join join = new Join();
+
+            // When
+            List<IDocument> results = join.Execute(null, context).ToList();
+
+            // Then
+            Assert.AreEqual(null, results.Single().Content);
+        }
+
+
+
+    }
+}
+
+/*
+ * TODO metadata unit test 
+
+- how should it work
+
+
+based on first file?
+or be all files?
+
+or should it be default
+
+ * 
+ * */
