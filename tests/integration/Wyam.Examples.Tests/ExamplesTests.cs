20fb6437b6f6402b0b01ea1ef1bc6cbbfe1578f2
Dave Glick <dave@daveaglick.com>
2017-03-15 14:15:25 -0400

    More work on getting packages and references all coordinated, all tests now pass (#473)

--- tests/integration/Wyam.Examples.Tests/ExamplesTests.cs
+++ tests/integration/Wyam.Examples.Tests/ExamplesTests.cs
@@ -11,71 +11,68 @@ using NUnit.Framework;
 
 namespace Wyam.Examples.Tests
 {
-    namespace Wyam.Examples.Tests
+    [TestFixture(Category = "ExcludeFromAppVeyor")]
+    public class ExamplesTests
     {
-        [TestFixture(Category = "ExcludeFromAppVeyor")]
-        public class ExamplesTests
+        private static IEnumerable<string> _paths;
+
+        public static IEnumerable<string> Paths
         {
-            private static IEnumerable<string> _paths;
-
-            public static IEnumerable<string> Paths
+            get
             {
-                get
+                if (_paths == null)
                 {
-                    if (_paths == null)
+                    string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+                    string examplesPath = Path.Combine(rootPath, "examples");
+                    while (!Directory.Exists(examplesPath))
                     {
-                        string rootPath = AppDomain.CurrentDomain.BaseDirectory;
-                        string examplesPath = Path.Combine(rootPath, "examples");
-                        while (!Directory.Exists(examplesPath))
-                        {
-                            rootPath = Path.GetDirectoryName(rootPath);
-                            examplesPath = Path.Combine(rootPath, "examples");
-                        }
-
-                        _paths = Directory.EnumerateDirectories(examplesPath);
+                        rootPath = Path.GetDirectoryName(rootPath);
+                        examplesPath = Path.Combine(rootPath, "examples");
                     }
 
-                    return _paths;
+                    _paths = Directory.EnumerateDirectories(examplesPath);
                 }
+
+                return _paths;
             }
+        }
 
-            [OneTimeSetUp]
-            public void SetUp()
-            {
-                Directory.CreateDirectory(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"));
-            }
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Directory.CreateDirectory(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"));
+        }
 
-            [OneTimeTearDown]
-            public void TearDown()
-            {
-                Directory.Delete(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"), true);
-            }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Directory.Delete(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"), true);
+        }
 
-            [Test]
-            [TestCaseSource(typeof(ExamplesTests), nameof(Paths))]
-            public void ExecuteExample(string example)
-            {
-                // Given
-                string packagesPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "packages");
-                TestContext.Out.WriteLine($"Packages path: {packagesPath}");
+        [Test]
+        [TestCaseSource(typeof(ExamplesTests), nameof(Paths))]
+        public void ExecuteExample(string example)
+        {
+            // Given
+            string packagesPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "packages");
+            TestContext.Out.WriteLine($"Packages path: {packagesPath}");
 
-                // When
-                Process process = new Process();
-                process.StartInfo.FileName = Path.Combine(TestContext.CurrentContext.TestDirectory, "Wyam.exe");
-                process.StartInfo.Arguments = $@"--use-local-packages --packages-path ""{packagesPath}"" ""{example}""";
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.OutputDataReceived += (s, e) => TestContext.Out.WriteLine(e.Data);
-                process.ErrorDataReceived += (s, e) => TestContext.Out.WriteLine(e.Data);
-                process.Start();
-                process.BeginOutputReadLine();
-                process.WaitForExit();
+            // When
+            Process process = new Process();
+            process.StartInfo.FileName = Path.Combine(TestContext.CurrentContext.TestDirectory, "Wyam.exe");
+            process.StartInfo.Arguments = $@"--use-local-packages --packages-path ""{packagesPath}"" ""{example}""";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += (s, e) => TestContext.Out.WriteLine(e.Data);
+            process.ErrorDataReceived += (s, e) => TestContext.Out.WriteLine(e.Data);
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
 
-                // Then
-                Assert.AreEqual(0, process.ExitCode);
-            }
+            // Then
+            Assert.AreEqual(0, process.ExitCode);
         }
     }
 }
