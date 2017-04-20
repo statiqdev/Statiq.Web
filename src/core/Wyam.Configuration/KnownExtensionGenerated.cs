4aba8ee22c4a9a1498bb35a58b9b727f086b69fd
Dave Glick <dave@daveaglick.com>
2017-04-20 10:55:05 -0400

    Moved recipes to a dedicated folder

--- src/core/Wyam.Configuration/KnownExtensionGenerated.cs
+++ src/core/Wyam.Configuration/KnownExtensionGenerated.cs
@@ -10,10 +10,7 @@ namespace Wyam.Configuration
 		public static readonly KnownExtension JavaScript = new KnownExtension("Wyam.Testing.JavaScript");
 		public static readonly KnownExtension All = new KnownExtension("Wyam.All");
 		public static readonly KnownExtension AmazonWebServices = new KnownExtension("Wyam.AmazonWebServices");
-		public static readonly KnownExtension Blog = new KnownExtension("Wyam.Blog");
-		public static readonly KnownExtension BookSite = new KnownExtension("Wyam.BookSite");
 		public static readonly KnownExtension CodeAnalysis = new KnownExtension("Wyam.CodeAnalysis");
-		public static readonly KnownExtension Docs = new KnownExtension("Wyam.Docs");
 		public static readonly KnownExtension Feeds = new KnownExtension("Wyam.Feeds");
 		public static readonly KnownExtension Git = new KnownExtension("Wyam.Git");
 		public static readonly KnownExtension GitHub = new KnownExtension("Wyam.GitHub");
@@ -28,10 +25,13 @@ namespace Wyam.Configuration
 		public static readonly KnownExtension SearchIndex = new KnownExtension("Wyam.SearchIndex");
 		public static readonly KnownExtension Tables = new KnownExtension("Wyam.Tables");
 		public static readonly KnownExtension TextGeneration = new KnownExtension("Wyam.TextGeneration");
-		public static readonly KnownExtension WebRecipe = new KnownExtension("Wyam.WebRecipe");
 		public static readonly KnownExtension Xmp = new KnownExtension("Wyam.Xmp");
 		public static readonly KnownExtension Xslt2 = new KnownExtension("Wyam.Xslt2");
 		public static readonly KnownExtension Yaml = new KnownExtension("Wyam.Yaml");
+		public static readonly KnownExtension Blog = new KnownExtension("Wyam.Blog");
+		public static readonly KnownExtension BookSite = new KnownExtension("Wyam.BookSite");
+		public static readonly KnownExtension Docs = new KnownExtension("Wyam.Docs");
+		public static readonly KnownExtension WebRecipe = new KnownExtension("Wyam.WebRecipe");
 		public static readonly KnownExtension CleanBlogTheme = new KnownExtension("Wyam.Blog.CleanBlog");
 		public static readonly KnownExtension PhantomTheme = new KnownExtension("Wyam.Blog.Phantom");
 		public static readonly KnownExtension VelocityTheme = new KnownExtension("Wyam.BookSite.Velocity");
