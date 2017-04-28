bff8c6d2a1407b1525c6f125428b59fa1d35643f
RLittlesII <rodney.littles@gmail.com>
2017-04-28 06:29:57 -0500

    Added Solid State theme

--- src/core/Wyam.Configuration/KnownThemeGenerated.cs
+++ src/core/Wyam.Configuration/KnownThemeGenerated.cs
@@ -4,6 +4,7 @@ namespace Wyam.Configuration
     {
 		public static readonly KnownTheme CleanBlog = new KnownTheme(nameof(KnownRecipe.Blog), null, new[] { "Wyam.Blog.CleanBlog" });
 		public static readonly KnownTheme Phantom = new KnownTheme(nameof(KnownRecipe.Blog), null, new[] { "Wyam.Blog.Phantom" });
+		public static readonly KnownTheme SolidState = new KnownTheme(nameof(KnownRecipe.Blog), null, new[] { "Wyam.Blog.SolidState" });
 		public static readonly KnownTheme Velocity = new KnownTheme(nameof(KnownRecipe.BookSite), null, new[] { "Wyam.BookSite.Velocity" });
 		public static readonly KnownTheme Samson = new KnownTheme(nameof(KnownRecipe.Docs), null, new[] { "Wyam.Docs.Samson" });
     }
