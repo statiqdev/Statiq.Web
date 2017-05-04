4ad428566c03b382a6642f6fc7b4e213dc737a7b
Dave Glick <dave@daveaglick.com>
2017-05-04 18:17:02 -0400

    Less module now prepends underscore and/or appends .less if included file can't be found, similar to Sass module (#507)

--- src/extensions/Wyam.Less/FileSystemReader.cs
+++ src/extensions/Wyam.Less/FileSystemReader.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using dotless.Core.Input;
 using Wyam.Common.IO;
 
@@ -14,8 +15,8 @@ namespace Wyam.Less
 
         public byte[] GetBinaryFileContents(string fileName)
         {
-            IFile file = _fileSystem.GetInputFile(fileName);
-            using (var stream = file.OpenRead())
+            IFile file = GetInputFile(fileName);
+            using (Stream stream = file.OpenRead())
             {
                 byte[] buffer = new byte[stream.Length];
                 stream.Read(buffer, 0, (int)stream.Length);
@@ -23,12 +24,57 @@ namespace Wyam.Less
             }
         }
 
-        public string GetFileContents(string fileName) =>
-            _fileSystem.GetInputFile(fileName).ReadAllText();
+        public string GetFileContents(string fileName) => GetInputFile(fileName).ReadAllText();
 
-        public bool DoesFileExist(string fileName) =>
-            _fileSystem.GetInputFile(fileName).Exists;
+        public bool DoesFileExist(string fileName) => GetInputFile(fileName).Exists;
 
         public bool UseCacheDependencies => true;
+
+        private IFile GetInputFile(FilePath filePath)
+        {
+            // Find the requested file
+            // ...as specified
+            IFile file = _fileSystem.GetInputFile(filePath);
+            if (file.Exists)
+            {
+                return file;
+            }
+
+            // ...with extension (if not already)
+            if (!filePath.HasExtension || filePath.Extension != ".less")
+            {
+                FilePath extensionPath = filePath.AppendExtension(".less");
+                IFile extensionFile = _fileSystem.GetInputFile(extensionPath);
+                if (extensionFile.Exists)
+                {
+                    return extensionFile;
+                }
+
+                // ...and with underscore prefix (if not already)
+                if (!extensionPath.FileName.FullPath.StartsWith("_"))
+                {
+                    extensionPath = extensionPath.Directory.CombineFile("_" + extensionPath.FileName.FullPath);
+                    extensionFile = _fileSystem.GetInputFile(extensionPath);
+                    if (extensionFile.Exists)
+                    {
+                        return extensionFile;
+                    }
+                }
+            }
+
+            // ...with underscore prefix (if not already)
+            if (!filePath.FileName.FullPath.StartsWith("_"))
+            {
+                filePath = filePath.Directory.CombineFile("_" + filePath.FileName.FullPath);
+                IFile underscoreFile = _fileSystem.GetInputFile(filePath);
+                if (underscoreFile.Exists)
+                {
+                    return underscoreFile;
+                }
+            }
+
+            // Can't find it, default to the original
+            return file;
+        }
     }
 }
