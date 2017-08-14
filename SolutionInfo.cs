b55198da860ed6ead06814ce475030e3351b88fe
Dave Glick <dave@daveaglick.com>
2017-08-14 13:27:04 -0400

    Release prep

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Wyam")]
-[assembly: AssemblyVersion("0.18.5")]
-[assembly: AssemblyFileVersion("0.18.5")]
-[assembly: AssemblyInformationalVersion("0.18.5")]
+[assembly: AssemblyVersion("0.18.6")]
+[assembly: AssemblyFileVersion("0.18.6")]
+[assembly: AssemblyInformationalVersion("0.18.6")]
 [assembly: AssemblyCopyright("Copyright © Wyam Contributors")]
 
