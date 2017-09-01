4410625d630448326f54b74f3980febf3cf3fa48
Dave Glick <dave@daveaglick.com>
2017-09-01 17:14:53 -0400

    Some cleanup for Stellar theme

--- src/core/Wyam.Configuration/KnownExtensionGenerated.cs
+++ src/core/Wyam.Configuration/KnownExtensionGenerated.cs
@@ -37,6 +37,7 @@ namespace Wyam.Configuration
 		public static readonly KnownExtension CleanBlogTheme = new KnownExtension("Wyam.Blog.CleanBlog");
 		public static readonly KnownExtension PhantomTheme = new KnownExtension("Wyam.Blog.Phantom");
 		public static readonly KnownExtension SolidStateTheme = new KnownExtension("Wyam.Blog.SolidState");
+		public static readonly KnownExtension StellarTheme = new KnownExtension("Wyam.Blog.Stellar");
 		public static readonly KnownExtension VelocityTheme = new KnownExtension("Wyam.BookSite.Velocity");
 		public static readonly KnownExtension SamsonTheme = new KnownExtension("Wyam.Docs.Samson");
     }
--- themes/Blog/Stellar/_README.md
+++ themes/Blog/Stellar/_README.md
@@ -1,4 +1,5 @@
 # Wyam Theme: Stellar
+
 This is a port of the [Html5up "Stellar"](https://html5up.net/stellar) design into a [wyam.io](https://wyam.io) site theme by Richard Burte (AreBee).
 
-Note the that the code components (the .cshtml files) are covered by the Unlicense. The theme CSS etc is covered by its own license, see [license.txt](LICENSE.TXT)
+Note the that the code components (the .cshtml files) are covered by the Unlicense. The theme CSS etc is covered by its own license, see [_LICENSE.txt](_LICENSE.TXT)
