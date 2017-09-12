294c594ca9624e8a063b55874570032e29a050c9
RLittlesII <rodney.littles@gmail.com>
2017-09-11 22:35:19 -0500

    Cake.Wyam - Added .NET Core target Added netstandard1.6 target Bumped Cake to 0.22.2

--- src/clients/Cake.Wyam/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cake.Wyam")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("690065cb-cecd-4df6-a547-38c96b1a7f55")]
