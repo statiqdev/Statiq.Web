b714bbadd9a3a81644b36b717995bce9c6d4ef98
Mark Lopez <m@silvenga.com>
2018-01-01 15:10:59 -0600

    Added coverage around UseExtensions bug.

---
+++ tests/extensions/Wyam.Markdown.Tests/SecondExternalMarkdownExtension.cs
@@ -0,0 +1,36 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace Wyam.Markdown.Tests
+{
+    public class SecondExternalMarkdownExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            pipeline.DocumentProcessed -= PipelineOnDocumentProcessed;
+            pipeline.DocumentProcessed += PipelineOnDocumentProcessed;
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+        }
+
+        private static void PipelineOnDocumentProcessed(MarkdownDocument document)
+        {
+            foreach (var node in document.Descendants())
+            {
+                if (node is Inline)
+                {
+                    var link = node as LinkInline;
+                    if (link != null && link.IsImage)
+                    {
+                        link.GetAttributes().AddClass("second");
+                    }
+                }
+            }
+        }
+    }
+}
