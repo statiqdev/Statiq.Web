fc48e043e46dfa463658cede87f248b646a1a511
Dave Glick <dave@daveaglick.com>
2018-04-09 17:32:34 -0400

    Fixes the list of known extensions (#652)

--- src/core/Wyam.Configuration/KnownExtension.cs
+++ src/core/Wyam.Configuration/KnownExtension.cs
@@ -18,6 +18,27 @@
         }
 
         public static readonly KnownExtension All = new KnownExtension("Wyam.All");
+
+        public static readonly KnownExtension AmazonWebServices = new KnownExtension("Wyam.AmazonWebServices");
+        public static readonly KnownExtension CodeAnalysis = new KnownExtension("Wyam.CodeAnalysis");
+        public static readonly KnownExtension Feeds = new KnownExtension("Wyam.Feeds");
+        public static readonly KnownExtension GitHub = new KnownExtension("Wyam.GitHub");
+        public static readonly KnownExtension Highlight = new KnownExtension("Wyam.Highlight");
+        public static readonly KnownExtension Html = new KnownExtension("Wyam.Html");
+        public static readonly KnownExtension Images = new KnownExtension("Wyam.Images");
+        public static readonly KnownExtension Json = new KnownExtension("Wyam.Json");
+        public static readonly KnownExtension Less = new KnownExtension("Wyam.Less");
+        public static readonly KnownExtension Markdown = new KnownExtension("Wyam.Markdown");
+        public static readonly KnownExtension Minification = new KnownExtension("Wyam.Minification");
+        public static readonly KnownExtension Razor = new KnownExtension("Wyam.Razor");
+        public static readonly KnownExtension Sass = new KnownExtension("Wyam.Sass");
+        public static readonly KnownExtension SearchIndex = new KnownExtension("Wyam.SearchIndex");
+        public static readonly KnownExtension Tables = new KnownExtension("Wyam.Tables");
+        public static readonly KnownExtension TextGeneration = new KnownExtension("Wyam.TextGeneration");
+        public static readonly KnownExtension Xmp = new KnownExtension("Wyam.Xmp");
+        public static readonly KnownExtension Yaml = new KnownExtension("Wyam.Yaml");
+        public static readonly KnownExtension YouTube = new KnownExtension("Wyam.YouTube");
+
         public static readonly KnownExtension BlogTemplateTheme = new KnownExtension("Wyam.Blog.BlogTemplate");
         public static readonly KnownExtension CleanBlogTheme = new KnownExtension("Wyam.Blog.CleanBlog");
         public static readonly KnownExtension PhantomTheme = new KnownExtension("Wyam.Blog.Phantom");
