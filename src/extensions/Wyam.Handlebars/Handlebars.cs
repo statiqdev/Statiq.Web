93ff72776e7f99d622b5c5f33f50ed4d6bc203b6
Jêrome Rx <jerome+git@rouaix.eu>
2018-04-22 15:23:48 +0200

    Handlebars module

---
+++ src/extensions/Wyam.Handlebars/Handlebars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Common.Modules;
+using HDN = HandlebarsDotNet;
+
+namespace Wyam.Handlebars
+{
+    public class Handlebars : IModule
+    {
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            return inputs
+                .AsParallel()
+                .Select(context, input =>
+                {
+                    var template = HDN.Handlebars.Compile(input.Content);
+                    var templateValues = new { metadata = input.Metadata.ToDictionary(kv => kv.Key, kv => kv.Value) };
+                    var output = template(templateValues);
+                    using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(output)))
+                    {
+                        var document = context.GetDocument(input, stream, disposeStream: false);
+                        return document;
+                    }
+                })
+                .Where(x => x != null);
+        }
+    }
+}
