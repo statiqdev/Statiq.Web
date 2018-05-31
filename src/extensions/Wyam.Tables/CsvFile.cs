76a1126e9a45eac1e93f47596936c3e171d14625
Dan Friedman <dan@fantasticfriedmans.com>
2018-05-30 21:24:27 -0500

    Replace Tabular with EPPlus and CsvHelper

---
+++ src/extensions/Wyam.Tables/CsvFile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Wyam.Tables
+{
+    internal static class CsvFile
+    {
+        public static IEnumerable<IEnumerable<string>> GetAllRecords(Stream stream, string delimiter = null)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                return GetAllRecords(reader, delimiter);
+            }
+        }
+
+        public static IEnumerable<IEnumerable<string>> GetAllRecords(TextReader reader, string delimiter = null)
+        {
+            List<IEnumerable<string>> records = new List<IEnumerable<string>>();
+            var configuration = delimiter == null ? new Configuration { HasHeaderRecord = false } : new Configuration { HasHeaderRecord = false, Delimiter = delimiter };
+
+            using (var csv = new CsvReader(reader, configuration))
+            {
+                while (csv.Read())
+                {
+                    var currentRecord = csv.Context.Record;
+                    records.Add(currentRecord);
+                }
+            }
+
+            return records;
+        }
+
+        public static void WriteAllRecords(IEnumerable<IEnumerable<string>> records, Stream stream)
+        {
+            var writer = new StreamWriter(stream);
+            WriteAllRecords(records, writer);
+            writer.Flush();
+        }
+
+        public static void WriteAllRecords(IEnumerable<IEnumerable<string>> records, TextWriter writer)
+        {
+            if (records == null)
+            {
+                return;
+            }
+
+            var csv = new CsvWriter(writer, new Configuration { QuoteAllFields = true });
+            {
+                foreach (var row in records)
+                {
+                    foreach (var cell in row)
+                    {
+                        csv.WriteField(cell ?? string.Empty);
+                    }
+                    csv.NextRecord();
+                }
+            }
+        }
+    }
+}
---
+++ src/extensions/Wyam.Tables/ExcelFile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OfficeOpenXml;
+
+namespace Wyam.Tables
+{
+    internal static class ExcelFile
+    {
+        public static IEnumerable<IEnumerable<string>> GetAllRecords(Stream stream, int sheetNumber = 1)
+        {
+            using (var excel = new ExcelPackage(stream))
+            {
+                if (sheetNumber > excel.Workbook.Worksheets.Count)
+                {
+                    return null;
+                }
+
+                var sheet = excel.Workbook.Worksheets[sheetNumber];
+
+                return GetAllRecords(sheet);
+            }
+        }
+
+        public static IEnumerable<IEnumerable<string>> GetAllRecords(ExcelWorksheet sheet)
+        {
+            var dimension = sheet.Dimension;
+
+            if (dimension == null)
+            {
+                return null;
+            }
+
+            var rowList = new List<List<string>>();
+            int rowCount = dimension.Rows;
+            int columnCount = dimension.Columns;
+
+            for (var r = 1; r <= rowCount; r++)
+            {
+                var rowValues = new List<string>(columnCount);
+                for (var c = 1; c <= columnCount; c++)
+                {
+                    var cell = sheet.Cells[r, c].FirstOrDefault();
+                    rowValues.Add(cell?.Value?.ToString());
+                }
+
+                rowList.Add(rowValues);
+            }
+
+            return rowList;
+        }
+    }
+}
