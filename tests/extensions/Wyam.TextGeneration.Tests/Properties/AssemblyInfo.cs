e450db405fdb0f42583bc1f30610a22f037cad72
Dave Glick <dave@daveaglick.com>
2018-06-06 11:13:43 -0400

    Removing TextGeneration module and examples, now deprecated

--- tests/extensions/Wyam.TextGeneration.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Wyam.TextGeneration.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("704624af-5081-4ec7-bd47-ab93292d7b4e")]
