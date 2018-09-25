864a02ff0c4a93f300c49a67e7815b7849471a6b
Jêrome Rx <jerome+git@rouaix.eu>
2018-09-25 23:44:08 +0200

    OpenApi work in progress

---
+++ tests/integration/Wyam.Examples.Tests/EmbededExamplesTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Wyam.Common.IO;
+using Wyam.Configuration;
+using Wyam.Core.Execution;
+
+namespace Wyam.Examples.Tests
+{
+    [TestFixture]
+    //[TestFixture(Category = "ExcludeFromAppVeyor")]
+    public class EmbededExamplesTests
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Directory.CreateDirectory(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"));
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Directory.Delete(Path.Combine(TestContext.CurrentContext.TestDirectory, "packages"), true);
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        [TestCaseSource(typeof(ExamplesTests), nameof(ExamplesTests.Paths))]
+        public void ExecuteExample(string example)
+        {
+            var engine = new Engine();
+            var configurator = new Configurator(engine);
+            configurator.Configure(new FilePath(example));
+
+            engine.Execute();
+        }
+    }
+}
