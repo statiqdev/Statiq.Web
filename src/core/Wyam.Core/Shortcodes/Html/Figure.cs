de4927dec6e5fd67c130a56543fc88aa87adf97e
Dave Glick <dave@daveaglick.com>
2019-02-15 10:31:05 -0500

    Figure shortcode

--- src/core/Wyam.Core/Shortcodes/Html/Figure.cs
+++ src/core/Wyam.Core/Shortcodes/Html/Figure.cs
@@ -20,20 +20,45 @@ namespace Wyam.Core.Shortcodes.Html
                 "Target",
                 "Rel",
                 "Alt",
-                "Title",
-                "Caption",
                 "Class",
                 "Height",
-                "Width",
-                "Attr",
-                "AttrLink");
+                "Width");
 
             XElement figure = new XElement(
                 "figure",
-                arguments.XElement("img", "Src", src => new[]
-                {
-                    new XAttribute("src", context.GetLink(src))
-                }));
+                arguments.XAttribute("class"));
+
+            // Image link
+            XElement imageLink = arguments.XElement("a", "link", x => new[]
+            {
+                new XAttribute("href", context.GetLink(x)),
+                arguments.XAttribute("target"),
+                arguments.XAttribute("rel")
+            });
+
+            // Image
+            XElement image = arguments.XElement("img", "src", x => new[]
+            {
+                new XAttribute("src", context.GetLink(x)),
+                arguments.XAttribute("alt"),
+                arguments.XAttribute("height"),
+                arguments.XAttribute("width")
+            });
+            if (imageLink != null && image != null)
+            {
+                imageLink.Add(image);
+                figure.Add(imageLink);
+            }
+            else if (image != null)
+            {
+                figure.Add(image);
+            }
+
+            // Caption
+            if (content != null)
+            {
+                figure.Add(new XElement("figcaption", content));
+            }
 
             return context.GetShortcodeResult(figure.ToString());
         }
