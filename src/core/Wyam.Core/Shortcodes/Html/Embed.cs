1795ccf5655d0107a39b37385d35f51a994f0fd8
Dave Glick <dave@daveaglick.com>
2019-02-17 19:36:09 -0500

    General oEmbed shortcode

---
+++ src/core/Wyam.Core/Shortcodes/Html/Embed.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Wyam.Common.Documents;
+using Wyam.Common.Execution;
+using Wyam.Common.Meta;
+using Wyam.Common.Shortcodes;
+using Wyam.Common.Util;
+
+namespace Wyam.Core.Shortcodes.Html
+{
+    /// <summary>
+    /// Calls an oEmbed endpoint and renders the embedded content (see https://oembed.com/).S
+    /// </summary>
+    public class Embed : IShortcode
+    {
+        // Cache the HttpClient as per recommended advice. Since this is short lived we don't have to worry about DNS issues.
+        private static readonly HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) };
+
+        public virtual IShortcodeResult Execute(KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context)
+        {
+            string value = args.SingleValue();
+
+            // Get the oEmbed response
+            HttpResponseMessage response = _httpClient.GetAsync(value).Result;
+            response.EnsureSuccessStatusCode();
+            EmbedResponse embedResponse;
+            if (response.Content.Headers.ContentType.MediaType == "application/json"
+                || response.Content.Headers.ContentType.MediaType == "text/html")
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(EmbedResponse));
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                {
+                    embedResponse = (EmbedResponse)serializer.ReadObject(stream);
+                }
+            }
+            else if (response.Content.Headers.ContentType.MediaType == "text/xml")
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(EmbedResponse));
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                {
+                    embedResponse = (EmbedResponse)serializer.ReadObject(stream);
+                }
+            }
+            else
+            {
+                throw new InvalidDataException("Unknown content type for oEmbed response");
+            }
+
+            // Switch based on type
+            if (!string.IsNullOrEmpty(embedResponse.Html))
+            {
+                return context.GetShortcodeResult(embedResponse.Html);
+            }
+            else if (embedResponse.Type == "photo")
+            {
+                if (string.IsNullOrEmpty(embedResponse.Url)
+                    || string.IsNullOrEmpty(embedResponse.Width)
+                    || string.IsNullOrEmpty(embedResponse.Height))
+                {
+                    throw new InvalidDataException("Did not receive required oEmbed values for image type");
+                }
+                return context.GetShortcodeResult($"<img src=\"{embedResponse.Url}\" width=\"{embedResponse.Width}\" height=\"{embedResponse.Height}\" />");
+            }
+            else if (embedResponse.Type == "link")
+            {
+                if (!string.IsNullOrEmpty(embedResponse.Title))
+                {
+                    return context.GetShortcodeResult($"<a href=\"{value}\">{embedResponse.Title}</a>");
+                }
+                return context.GetShortcodeResult($"<a href=\"{value}\">{value}</a>");
+            }
+
+            throw new InvalidDataException("Could not determine embedded content for oEmbed response");
+        }
+
+        [DataContract(Name = "oembed", Namespace = "")]
+        public class EmbedResponse
+        {
+            [DataMember(Name = "type")]
+            public string Type { get; set; }
+
+            [DataMember(Name = "url")]
+            public string Url { get; set; }
+
+            [DataMember(Name = "title")]
+            public string Title { get; set; }
+
+            [DataMember(Name = "width")]
+            public string Width { get; set; }
+
+            [DataMember(Name = "height")]
+            public string Height { get; set; }
+
+            [DataMember(Name = "html")]
+            public string Html { get; set; }
+        }
+    }
+}
--- src/core/Wyam.Core/Shortcodes/Html/Twitter.cs
+++ src/core/Wyam.Core/Shortcodes/Html/Twitter.cs
@@ -13,14 +13,11 @@ using Wyam.Common.Util;
 
 namespace Wyam.Core.Shortcodes.Html
 {
-    public class Twitter : IShortcode
+    public class Twitter : Embed
     {
-        // Cache the HttpClient as per recommended advice. Since this is short lived we don't have to worry about DNS issues.
-        private static readonly HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) };
-
         private bool _omitScript;
 
-        public IShortcodeResult Execute(KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context)
+        public override IShortcodeResult Execute(KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context)
         {
             ConvertingDictionary arguments = args.ToDictionary(
                 context,
@@ -49,27 +46,19 @@ namespace Wyam.Core.Shortcodes.Html
                 query.Add("omit_script=true");
             }
             string queryString = query.Count > 0 ? "?" + string.Join("&", query) : null;
-            string url = $"https://publish.twitter.com/oembed?url=https://twitter.com/username/status/{arguments.String("Id")}{query}";
-
-            // Get the oEmbed response with the HTML to embed
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Embed));
-            Embed embed;
-            using (Stream stream = _httpClient.GetStreamAsync(url).Result)
-            {
-                embed = (Embed)serializer.ReadObject(stream);
-            }
+            string url = $"https://publish.twitter.com/oembed?url=https://twitter.com/username/status/{arguments.String("Id")}{queryString}";
 
             // Omit the script on the next Twitter embed
             _omitScript = true;
 
-            return context.GetShortcodeResult(embed.Html);
-        }
-
-        [DataContract]
-        public class Embed
-        {
-            [DataMember(Name = "html")]
-            public string Html { get; set; }
+            return base.Execute(
+                new KeyValuePair<string, string>[]
+                {
+                    new KeyValuePair<string, string>(null, url)
+                },
+                content,
+                document,
+                context);
         }
     }
 }
