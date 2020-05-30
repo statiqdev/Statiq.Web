f81eee04149219f8d34a131ca4b3e42fc76d2c98
Dave Glick <dave@daveaglick.com>
2020-05-30 10:39:55 -0400

    Added a unit test for feed id fix

---
+++ tests/Statiq.Web.Tests/Pipelines/FeedsFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Testing;
+using Statiq.Web.Pipelines;
+
+namespace Statiq.Web.Tests.Pipelines
+{
+    [TestFixture]
+    [NonParallelizable]
+    public class FeedsFixture : BaseFixture
+    {
+        public class ExecuteTests : DataFixture
+        {
+            [Test]
+            public async Task AllowsStringIds()
+            {
+                // Given
+                Bootstrapper bootstrapper = Bootstrapper.Factory.CreateWeb(Array.Empty<string>());
+                TestFileProvider fileProvider = new TestFileProvider
+                {
+                    { "/input/foo.md", "Hi!" },
+                    {
+                        "/input/feed.yml",
+                        @"FeedItemId: => ""Bar""
+FeedRss: true"
+                    }
+                };
+
+                // When
+                BootstrapperTestResult result = await bootstrapper.RunTestAsync(fileProvider);
+
+                // Then
+                result.ExitCode.ShouldBe((int)ExitCode.Normal);
+                IDocument document = result.Outputs[nameof(Web.Pipelines.Feeds)][Phase.Process].ShouldHaveSingleItem();
+                (await document.GetContentStringAsync()).ShouldContain(@"<guid isPermaLink=""false"">Bar</guid>");
+            }
+        }
+    }
+}
