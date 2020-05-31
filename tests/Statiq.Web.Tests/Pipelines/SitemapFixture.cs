8cf48c02e30116b4ae6937eb3607aad5c7ab028b
Adam Shirt <adam.shirt@developmomentum.com>
2020-05-31 17:24:36 +0100

    Sitemap inclusion metadata

---
+++ tests/Statiq.Web.Tests/Pipelines/SitemapFixture.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Testing;
+using Statiq.Web.Pipelines;
+
+namespace Statiq.Web.Tests.Pipelines
+{
+    [TestFixture]
+    [NonParallelizable]
+    public class SitemapFixture
+    {
+        public class ExecuteTests : DataFixture
+        {
+            [Test]
+            public async Task IncludeInSitemapByDefault()
+            {
+                // Given
+                Bootstrapper bootstrapper = Bootstrapper
+                    .Factory
+                    .CreateWeb(Array.Empty<string>());
+
+                TestFileProvider fileProvider = new TestFileProvider
+                {
+                    {
+                        "/input/foo.html",
+                        ContentFile
+                    }
+                };
+
+                // When
+                BootstrapperTestResult result = await bootstrapper.RunTestAsync(fileProvider);
+
+                // Then
+                result.ExitCode.ShouldBe((int)ExitCode.Normal);
+                IDocument document = result.Outputs[nameof(Sitemap)][Phase.Output].ShouldHaveSingleItem();
+                string sitemapContent = await document.GetContentStringAsync();
+                sitemapContent.ShouldContain("<loc>/foo</loc>");
+            }
+
+            [Test]
+            public async Task IncludeInSitemapWhenSpecified()
+            {
+                // Given
+                Bootstrapper bootstrapper = Bootstrapper
+                    .Factory
+                    .CreateWeb(Array.Empty<string>())
+                    .AddSetting(WebKeys.GenerateSitemap, true);
+
+                TestFileProvider fileProvider = new TestFileProvider
+                {
+                    {
+                        "/input/foo.html",
+                        @"IncludeInSitemap: true
+---" + ContentFile
+                    }
+                };
+
+                // When
+                BootstrapperTestResult result = await bootstrapper.RunTestAsync(fileProvider);
+
+                // Then
+                result.ExitCode.ShouldBe((int)ExitCode.Normal);
+                IDocument document = result.Outputs[nameof(Sitemap)][Phase.Output].ShouldHaveSingleItem();
+                string sitemapContent = await document.GetContentStringAsync();
+                sitemapContent.ShouldContain("<loc>/foo</loc>");
+            }
+
+            [Test]
+            public async Task ExcludeFromSitemapWhenSpecified()
+            {
+                // Given
+                Bootstrapper bootstrapper = Bootstrapper
+                    .Factory
+                    .CreateWeb(Array.Empty<string>())
+                    .AddSetting(WebKeys.GenerateSitemap, true);
+
+                TestFileProvider fileProvider = new TestFileProvider
+                {
+                    {
+                        "/input/foo.html",
+                        @"IncludeInSitemap: false
+---" + ContentFile
+                    }
+                };
+
+                // When
+                BootstrapperTestResult result = await bootstrapper.RunTestAsync(fileProvider);
+
+                // Then
+                result.ExitCode.ShouldBe((int)ExitCode.Normal);
+                IDocument document = result.Outputs[nameof(Sitemap)][Phase.Output].ShouldHaveSingleItem();
+                string sitemapContent = await document.GetContentStringAsync();
+                sitemapContent.ShouldNotContain("<loc>/foo</loc>");
+            }
+        }
+
+        public const string ContentFile = @"
+<html>
+  <head>
+  </head>
+  <body>
+  </body>
+</html>";
+    }
+}
