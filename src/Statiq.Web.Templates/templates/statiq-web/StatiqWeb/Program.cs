acdf86a50bf2ef33c7c1420cd20fe4f4bb15d13f
Mattias Karlsson <matkar@wcom.se>
2020-08-07 21:28:48 +0200

    Add Statiq.Web .NET CLI template

---
+++ src/Statiq.Web.Templates/templates/statiq-web/StatiqWeb/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace StatiqWeb
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
